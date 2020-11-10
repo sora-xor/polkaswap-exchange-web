d84678e8300ca1ecd6975fd044437888f462d395
Alex Natalia <38787212+alexnatalia@users.noreply.github.com>
2020-11-10 09:10:56 +0300

    Swap Interface Confirmation Screens (#8)
    * Added Transaction Submit dialog.
    * Swap Interface: Moved some variables to store for better relations between different screens and dialogs.
    * Confirm Swap Dialog: Fixed dialog opening, added confirmation flag to the store.
    * Added token default logo image, refactored translation call for liquidity.
    * Added SelectToken dialog on click.

---
+++ src/store/swap.ts
@@ -0,0 +1,125 @@
+import map from 'lodash/fp/map'
+import flatMap from 'lodash/fp/flatMap'
+import fromPairs from 'lodash/fp/fromPairs'
+import flow from 'lodash/fp/flow'
+import concat from 'lodash/fp/concat'
+
+import * as storage from '@/utils/storage'
+
+const types = flow(
+  flatMap(x => [x + '_REQUEST', x + '_SUCCESS', x + '_FAILURE']),
+  concat([
+    'GET_WALLET_CONNECTED',
+    'GET_TOKEN_FROM',
+    'GET_TOKEN_TO',
+    'GET_FROM_VALUE',
+    'GET_TO_VALUE',
+    'GET_TOKEN_FROM_PRICE',
+    'GET_SWAP_CONFIRM'
+  ]),
+  map(x => [x, x]),
+  fromPairs
+)([])
+
+function initialState () {
+  return {
+    isWalletConnected: false,
+    tokenFrom: null,
+    tokenTo: null,
+    fromValue: 0,
+    toValue: 0,
+    isTokenFromPrice: true,
+    slippageTolerance: 0.5,
+    liquidityProviderFee: 0.3,
+    isSwapConfirmed: false
+  }
+}
+
+const state = initialState()
+
+const getters = {
+  isWalletConnected (state) {
+    // TODO: Add Connect Wallet functionality
+    return !!storage.getItem('address')
+  },
+  tokenFrom (state) {
+    return state.tokenFrom
+  },
+  tokenTo (state) {
+    return state.tokenTo
+  },
+  fromValue (state) {
+    return state.fromValue
+  },
+  toValue (state) {
+    return state.toValue
+  },
+  isTokenFromPrice (state) {
+    return state.isTokenFromPrice
+  },
+  slippageTolerance (state) {
+    return state.slippageTolerance
+  },
+  liquidityProviderFee (state) {
+    return state.liquidityProviderFee
+  },
+  isSwapConfirmed (state) {
+    return state.isSwapConfirmed
+  }
+}
+
+const mutations = {
+  [types.GET_WALLET_CONNECTED] (state, isWalletConnected: boolean) {
+    state.isWalletConnected = isWalletConnected
+  },
+  [types.GET_TOKEN_FROM] (state, tokenFrom: any) {
+    state.tokenFrom = tokenFrom
+  },
+  [types.GET_TOKEN_TO] (state, tokenTo: any) {
+    state.tokenTo = tokenTo
+  },
+  [types.GET_FROM_VALUE] (state, fromValue: string | number) {
+    state.fromValue = fromValue
+  },
+  [types.GET_TO_VALUE] (state, toValue: string | number) {
+    state.toValue = toValue
+  },
+  [types.GET_TOKEN_FROM_PRICE] (state, isTokenFromPrice: boolean) {
+    state.isTokenFromPrice = isTokenFromPrice
+  },
+  [types.GET_SWAP_CONFIRM] (state, isSwapConfirmed: boolean) {
+    state.isSwapConfirmed = isSwapConfirmed
+  }
+}
+
+const actions = {
+  connectWallet ({ commit }, address: string) {
+    storage.setItem('address', address)
+    commit(types.GET_WALLET_CONNECTED, true)
+  },
+  setTokenFrom ({ commit }, token: any) {
+    commit(types.GET_TOKEN_FROM, token)
+  },
+  setTokenTo ({ commit }, token: any) {
+    commit(types.GET_TOKEN_TO, token)
+  },
+  setFromValue ({ commit }, fromValue: string | number) {
+    commit(types.GET_FROM_VALUE, fromValue)
+  },
+  setToValue ({ commit }, toValue: string | number) {
+    commit(types.GET_TO_VALUE, toValue)
+  },
+  setTokenFromPrice ({ commit }, isTokenFromPrice: boolean) {
+    commit(types.GET_TOKEN_FROM_PRICE, isTokenFromPrice)
+  },
+  setSwapConfirm ({ commit }, isSwapConfirmed: boolean) {
+    commit(types.GET_SWAP_CONFIRM, isSwapConfirmed)
+  }
+}
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions
+}
--- tests/unit/views/TransactionSubmit.spec.ts
+++ tests/unit/views/TransactionSubmit.spec.ts
@@ -1,7 +1,7 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import SoramitsuElements from '@soramitsu/soramitsu-js-ui'
-import Swap from '@/components/Swap.vue'
+import TransactionSubmit from '@/components/TransactionSubmit.vue'
 import { tokens } from '@/mocks/tokens'
 import { TranslationMock } from '../../utils'
 
@@ -9,40 +9,27 @@ const localVue = createLocalVue()
 localVue.use(Vuex)
 localVue.use(SoramitsuElements)
 
-describe('Swap.vue', () => {
-  let actions
+describe('TransactionSubmit.vue', () => {
   let getters
   let store
 
   beforeEach(() => {
-    TranslationMock(Swap)
-
-    actions = {
-      connectWallet: jest.fn(),
-      setTokenFrom: jest.fn(),
-      setTokenTo: jest.fn(),
-      setFromValue: jest.fn(),
-      setToValue: jest.fn(),
-      setTokenFromPrice: jest.fn()
-    }
+    TranslationMock(TransactionSubmit)
 
     getters = {
-      isWalletConnected: () => false,
       tokenFrom: () => tokens[0],
       tokenTo: () => tokens[1],
       fromValue: () => 100,
-      toValue: () => 45.4545,
-      isSwapConfirmed: () => false
+      toValue: () => 45.4545
     }
 
     store = new Vuex.Store({
-      actions,
       getters
     })
   })
 
   it('should renders correctly', () => {
-    const wrapper = shallowMount(Swap, { localVue, store })
+    const wrapper = shallowMount(TransactionSubmit, { localVue, store })
     expect(wrapper.element).toMatchSnapshot()
   })
 })
