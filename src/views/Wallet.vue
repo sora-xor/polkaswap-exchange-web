8e59ecb37330f92dcc633dd01f96d3bd2f6b2a08
Stefan Popov <stefanpopov@list.ru>
2020-11-18 13:14:09 +0300

    Add wallet component

---
+++ src/views/Wallet.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wallet-container s-flex">
+    <sora-neo-wallet />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Mixins } from 'vue-property-decorator'
+
+import TranslationMixin from '@/components/mixins/TranslationMixin'
+
+@Component
+export default class Wallet extends Mixins(TranslationMixin) {
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/layout';
+@import '../styles/soramitsu-variables';
+
+$menu-height: 65px;
+
+.wallet-container {
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - #{$menu-height});
+}
+</style>
