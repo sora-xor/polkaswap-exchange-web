a10a3228012fbf97ee47bc88b3ed823003b56de5
Alex Natalia <38787212+alexnatalia@users.noreply.github.com>
2020-11-25 15:49:06 +0300

    PSS-172: Dynamic Page Title (#14)
    * Added dynamic page title.

--- src/lang/en/index.ts
+++ src/lang/en/index.ts
@@ -1,6 +1,18 @@
 import { PageNames, Topics } from '@/consts'
 
 export default {
+  pageTitle: {
+    [PageNames.About]: 'About',
+    [PageNames.Exchange]: 'Exchange',
+    [PageNames.Swap]: 'Swap',
+    [PageNames.Pool]: 'Pool',
+    [PageNames.Stats]: 'Stats',
+    [PageNames.Support]: 'Support',
+    [PageNames.Wallet]: 'Wallet',
+    [PageNames.CreatePair]: 'Create Pair',
+    [PageNames.AddLiquidity]: 'Add Liquidity',
+    [PageNames.RemoveLiquidity]: 'Remove Liquidity'
+  },
   mainMenu: {
     [PageNames.About]: 'About',
     [PageNames.Exchange]: 'Exchange',
