081a4cb0432492809d928c7ce702f52fe533b016
alexnatalia <alekseenkonn@gmail.com>
2020-12-08 16:09:25 +0300

    Fixed titles case.

--- src/lang/en/index.ts
+++ src/lang/en/index.ts
@@ -68,7 +68,7 @@ export default {
     liquidityProviderFeeTooltip: 'A portion of each trade ({liquidityProviderFee}%) goes to liquidity providers as a protocol incentive.',
     enterAmount: 'Enter an amount',
     insufficientBalance: 'Insufficient {tokenSymbol} balance',
-    confirmSwap: 'Confirm Swap',
+    confirmSwap: 'Confirm swap',
     swapOutputMessage: 'Output is estimated. You will receive at least {transactionValue} or the transaction will revert.',
     transactionMessage: '{tokenFromValue} for {tokenToValue}'
   },
