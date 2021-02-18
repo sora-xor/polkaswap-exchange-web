6951e952260306d73e18910fa9403596407bab55
Alex Natalia <38787212+alexnatalia@users.noreply.github.com>
2021-02-18 11:10:20 +0300

    PSS-400: WEB UI. Minimum receive/Maximum sold token not changed (#69)
    * Fixed min received/max sold symbol.

--- src/components/SwapInfo.vue
+++ src/components/SwapInfo.vue
@@ -20,7 +20,7 @@
           <s-icon name="info" size="16" />
         </s-tooltip>
         <span>{{ t(`swap.${isExchangeB ? 'maxSold' : 'minReceived'}`) }}</span>
-        <span class="swap-info-value">{{ minMaxReceived }}<span class="asset-title">{{ getAssetSymbolText(isExchangeB) }}</span></span>
+        <span class="swap-info-value">{{ minMaxReceived }}<span class="asset-title">{{ getAssetSymbolText }}</span></span>
       </div>
       <!-- TODO: Hid for first iteration of development -->
       <!-- <div class="swap-info">
@@ -35,7 +35,7 @@
           <s-icon name="info" size="16" />
         </s-tooltip>
         <span>{{ t('swap.liquidityProviderFee') }}</span>
-        <span class="swap-info-value">{{ liquidityProviderFee }}<span class="asset-title">{{ getAssetSymbolText() }}</span></span>
+        <span class="swap-info-value">{{ liquidityProviderFee }}<span class="asset-title">{{ xorSymbol }}</span></span>
       </div>
       <!-- TODO 4 alexnatalia: Show if logged in and have info about Network Fee -->
       <div v-if="connected" class="swap-info">
@@ -43,7 +43,7 @@
           <s-icon name="info" size="16" />
         </s-tooltip>
         <span>{{ t('swap.networkFee') }}</span>
-        <span class="swap-info-value">{{ networkFee }}<span class="asset-title">{{ getAssetSymbolText() }}</span></span>
+        <span class="swap-info-value">{{ networkFee }}<span class="asset-title">{{ xorSymbol }}</span></span>
       </div>
     </template>
   </div>
@@ -111,11 +111,15 @@ export default class SwapInfo extends Mixins(TranslationMixin) {
     return ''
   }
 
-  getAssetSymbolText (isXorSymbol = true): string {
-    if (isXorSymbol) {
-      return ' ' + KnownSymbols.XOR
+  get xorSymbol (): string {
+    return ' ' + KnownSymbols.XOR
+  }
+
+  get getAssetSymbolText (): string {
+    if (this.isExchangeB) {
+      return this.tokenFrom ? ` ${this.tokenFrom.symbol}` : ''
     }
-    return this.tokenTo ? ' ' + this.tokenTo.symbol : ''
+    return this.tokenTo ? ` ${this.tokenTo.symbol}` : ''
   }
 
   handleSwitchPrice (): void {
