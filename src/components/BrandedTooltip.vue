14f261671d14a9b642f3e6b98628cc6e680d72c4
Stefan Popov <stefanpopov@list.ru>
2021-03-21 21:27:06 +0400

    Update BrandedTooltip.vue

--- src/components/BrandedTooltip.vue
+++ src/components/BrandedTooltip.vue
@@ -23,18 +23,40 @@ export default {}
 $tooltip-padding-top: $inner-spacing-mini * 0.5;
 $tooltip-padding-bottom: $inner-spacing-mini * 0.75;
 
-.el-tooltip__popper.is-light.info-tooltip {
+.el-tooltip__popper.is-light.s-border-radius-small /* .info-tooltip TODO: use it everywhere */ {
   max-width: 236px;
   padding: $tooltip-padding-top $inner-spacing-mini $tooltip-padding-bottom;
   line-height: $s-line-height-base;
   background-color: var(--s-color-brand-day);
   color: var(--s-color-utility-body);
-
   .popper__arrow {
     border-bottom-color: var(--s-color-brand-day);
-
-    &:after {
+    &::after {
       border-bottom-color: inherit;
+    }
+  }
+  &[x-placement^="top"] .popper__arrow {
+    border-top-color: var(--s-color-brand-day);
+    &::after {
+      border-top-color: inherit;
+    }
+  }
+  &[x-placement^="bottom"] .popper__arrow {
+    border-bottom-color: var(--s-color-brand-day);
+    &::after {
+      border-top-color: inherit;
+    }
+  }
+  &[x-placement^="right"] .popper__arrow {
+    border-right-color: var(--s-color-brand-day);
+    &::after {
+      border-right-color: inherit;
+    }
+  }
+  &[x-placement^="left"] .popper__arrow {
+    border-left-color: var(--s-color-brand-day);
+    &::after {
+      border-left-color: inherit;
     }
   }
 }
