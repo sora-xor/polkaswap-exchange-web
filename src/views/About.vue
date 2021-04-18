906e65e2ece37c4d3ece26fb14ce0b3fd5913d6e
Pobepto <roobertdantes@gmail.com>
2021-04-18 22:46:52 +0300

    feat: about page implementation

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,37 +1,79 @@
 <template>
-  <div class="layout s-flex">
-    <!-- TODO: Move the text below above the banner for mobile devices, add background layer with opacity for the banner -->
-    <!--div class="banner"/-->
+  <div class="layout">
     <div class="content">
-      <div class="terms s-flex">
-        <h1 class="title">{{ t('about.polkaswapText') }}</h1>
-        <i class="web3-logo" />
+      <div class="about-1">
+        <img src="@/assets/about/about01.png" draggable="false" class="img unselectable"/>
+        <div class="about-1-text">
+          <span class="text">Cross-chain exchange of assets, simple creation and listing of new assets, and the easiest swaps ever. The interoperable future of Polkadot is here and we are at the forefront!</span>
+        </div>
+        <img src="@/assets/about/web3.png" draggable="false" class="web3 unselectable"/>
       </div>
-      <!--div class="links s-flex">
-        <s-button type="primary" @click="handleClickExchange">
-          {{ t('about.openExchange') }}
-        </s-button>
-        <s-button type="link" icon="external-link-16" @click="handleGoToMedium">
-          {{ t('about.mediumLink') }}
-        </s-button>
-        <s-button type="link" icon="external-link-16" @click="handleGoToGithub">
-          {{ t('about.githubLink') }}
-        </s-button>
-      </div-->
-      <div class="articles s-flex">
-        <s-row>
-          <s-col :lg="3" :md="4" :sm="6" :xs="12" v-for="topic in AboutTopics" :key="topic.title">
-            <s-card class="article-card" border-radius="big" shadow="never">
-              <template #header>
-                <h4 class="title">
-                  <s-icon :size="24" :name="topic.icon" />
-                  {{ t(`about.${topic.title}.title`) }}
-                </h4>
-              </template>
-              <span>{{ t(`about.${topic.title}.text`) }}</span>
-            </s-card>
-          </s-col>
-        </s-row>
+      <div class="about-2">
+        <img src="@/assets/about/about02.png" draggable="false" class="unselectable" style="width: 60%">
+        <div class="about-2-text-1">
+          <span class="text">Polkaswap is a non-custodial, cross-chain AMM DEX protocol for swapping tokens based on Polkadot and Kusama relay chains, Polkadot and Kusama parachains, and blockchains directly connected via bridges.</span>
+        </div>
+        <div class="about-2-text-2">
+          <span class="text">Polkaswap removes trusted intermediaries and provides the opportunity for faster trading</span>
+        </div>
+        <div class="about-2-text-3">
+          <span class="text">Polkaswap is a community project and devs collaborate on the open source code using the BSD 4-clause license</span>
+        </div>
+      </div>
+      <div class="about-3">
+        <img src="@/assets/about/about03.png" draggable="false" class="unselectable" style="width: 60%">
+        <div class="about-3-text-1">
+          <span class="text">Polkaswap combines multiple liquidity sources under a common liquidity aggregation algorithm, operating completely on-chain, in a trustless and decentralized way.</span>
+        </div>
+        <div class="about-3-text-2">
+          <span class="text">When traders call the swap function, the liquidity aggregation algorithm will fill orders using the best offers across all liquidity sources. Liquidity sources can be other DEXs, order books, or other API-driven sources.</span>
+        </div>
+        <div class="about-3-text-3">
+          <span class="text">Because Polkaswap is an open-source project, anyone can add more liquidity sources by making contributions to the Polkaswap codebase.</span>
+        </div>
+      </div>
+      <div class="about-4">
+        <img src="@/assets/about/about04.png" draggable="false" class="unselectable" style="width: 60%">
+        <div class="about-4-text-1">
+          <span class="text">Go beyond the limits of current DEXs by adding tokens from the Polkadot ecosystem as well as other blockchains.</span>
+        </div>
+        <div class="about-4-text-2">
+          <span class="text">Create, list and trade your own tokens on the SORA network.</span>
+        </div>
+        <div class="about-4-text-3">
+          <span class="text">Our core infrastructure uses Parity Substrate, which is more scalable than Ethereum, and does not use expensive mining for consensus.</span>
+        </div>
+      </div>
+      <div class="about-5">
+        <img src="@/assets/about/about05.png" draggable="false" class="unselectable" style="width: 60%">
+        <div class="about-5-text-1">
+          <span class="text">PSWAP was created by community governance by voting on its release. It is a utility and governance token used to reward liquidity providers on Polkaswap. Unlike most other reward tokens, PSWAP is burned with transactions and decreases in supply over time.</span>
+        </div>
+        <div class="about-5-text-2">
+          <span class="text">The 0.3% fee for every swap on the Polkaswap DEX is used to buy back PSWAP tokens, which are then burned. At first, 90% of burned PSWAP tokens are reminted to allocate to liquidity providers, but with time this percentage will decrease to 35% after 4 years.</span>
+        </div>
+        <div class="about-5-text-3">
+          <span class="text"></span>
+        </div>
+      </div>
+      <div class="about-6">
+        <img src="@/assets/about/about06.png" draggable="false" class="unselectable" style="width: 70%; cursor: pointer;">
+      </div>
+      <div class="about-7">
+        <div class="about-7-part">
+          <img src="@/assets/about/about07-1.png" draggable="false" class="unselectable icon">
+          <span class="title">Become a SORA Validator<img src="@/assets/about/about07-3.png" class="link"></span>
+          <span class="text">Secure the future of the SORA network and decentralized apps like Polkaswap, and earn rewards along the way by becoming a SORA validator.</span>
+        </div>
+        <div class="about-7-part">
+          <img src="@/assets/about/about07-2.png" draggable="false" class="unselectable icon">
+          <span class="title">Connect a Chain to SORA<img src="@/assets/about/about07-3.png" class="link"></span>
+          <span class="text">Grow the decentralized world economy by connecting more chains to SORA and Polkaswap using the HASHI bridge protocol.</span>
+        </div>
+      </div>
+      <div class="about-8">
+        <img src="@/assets/about/about08.png" draggable="false" class="unselectable" style="width: 60%">
+        <span class="text" style="width: 55%; text-align: center;">Polkaswap is built on the SORA Network, focusing on interoperability to connect the rest of the crypto ecosystem to Polkadot.</span>
       </div>
     </div>
   </div>
@@ -62,164 +104,237 @@ export default class About extends Mixins(TranslationMixin) {
 }
 </script>
 
-<style lang="scss">
-.article-card {
-  .el-card__body {
-    line-height: $s-line-height-base;
-  }
-}
-</style>
-
 <style lang="scss" scoped>
-$logo-width: 140px;
-
-.layout {
+.content {
+  display: flex;
   flex-direction: column;
-  height: 100%;
-  .banner {
-    margin-top: $inner-spacing-mini;
-    margin-left: auto;
-    margin-right: auto;
-    width: calc(100% - #{$inner-spacing-mini} * 2);
-    height: 120px;
-    background-size: contain;
-    background-repeat: repeat-x;
-    flex-shrink: 0;
+  justify-content: center;
+  align-items: center;
+}
+.unselectable {
+  user-drag: none;
+  user-select: none;
+}
+.img {
+  max-width: 1600px;
+  width: 100%;
+}
+
+.web3 {
+  width: 100px;
+  position: absolute;
+  bottom: 0;
+  right: 5%;
+}
+
+.a {
+  background-image: url('~@/assets/about/about01.png');
+}
+
+.about {
+  &-1 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    position: relative;
+    &-text {
+      top: 60%;
+      width: 35%;
+
+      position: absolute;
+      text-align: center;
+    }
   }
-  .content {
-    padding: $inner-spacing-big $inner-spacing-mini;
-    .terms, .links {
-      padding-right: $inner-spacing-medium;
-      padding-left: $inner-spacing-medium;
+  &-2 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    position: relative;
+    &-text-1 {
+      top: 16%;
+      right: 12.5%;
+      width: 35%;
+
+      position: absolute;
     }
-    .terms {
-      margin-bottom: $inner-spacing-big;
-      .title {
-        margin-top: 0;
-        margin-right: $inner-spacing-mini;
-        margin-bottom: 0;
-        width: calc(100% - #{$logo-width} - #{$inner-spacing-medium});
-        color: var(--s-color-brand-day);
-        font-size: var(--s-heading5-font-size);
-        font-feature-settings: $s-font-feature-settings-title;
-        letter-spacing: $s-letter-spacing-mini;
-        font-weight: 600;
-      }
-      .web3-logo {
-        margin-left: auto;
-        margin-top: $inner-spacing-small;
-        background-image: url('~@/assets/img/web3-logo.svg');
-        width: $logo-width;
-        height: 48px;
-      }
+    &-text-2 {
+      top: 47%;
+      right: 20.5%;
+      width: 20%;
+
+      position: absolute;
     }
-    .links {
+    &-text-3 {
+      top: 68%;
+      right: 20.5%;
+      width: 20%;
+
+      position: absolute;
+    }
+  }
+  &-3 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    position: relative;
+    &-text-1 {
+      top: 49.5%;
+      left: 22%;
+      width: 28%;
+
+      position: absolute;
+    }
+    &-text-2 {
+      top: 65%;
+      left: 28.5%;
+      width: 37%;
+
+      position: absolute;
+    }
+    &-text-3 {
+      top: 81.5%;
+      left: 28.5%;
+      width: 34%;
+
+      position: absolute;
+    }
+  }
+  &-4 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    position: relative;
+    &-text-1 {
+      top: 20%;
+      right: 25%;
+      width: 23%;
+
+      position: absolute;
+    }
+    &-text-2 {
+      top: 49%;
+      right: 20.5%;
+      width: 20%;
+
+      position: absolute;
+    }
+    &-text-3 {
+      top: 66%;
+      right: 20.5%;
+      width: 20%;
+
+      position: absolute;
+    }
+  }
+  &-5 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    position: relative;
+    &-text-1 {
+      top: 30%;
+      right: 35.5%;
+      width: 31%;
+
+      position: absolute;
+    }
+    &-text-2 {
+      top: 56%;
+      right: 19.5%;
+      width: 39%;
+
+      position: absolute;
+    }
+    &-text-3 {
+      top: 66%;
+      right: 20.5%;
+      width: 20%;
+
+      position: absolute;
+    }
+  }
+  &-6 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    position: relative;
+  }
+  &-7 {
+    width: 55%;
+    padding-top: 7rem;
+
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr;
+    gap: 0px 5rem;
+    grid-template-areas: ". .";
+    &-part {
       display: flex;
-      align-items: center;
-    }
-    .s-primary {
-      & + .s-link {
-        margin-left: $inner-spacing-medium;
+      flex-direction: column;
+
+      &:after {
+        content: '';
+        margin-top: 20px;
+        background: linear-gradient(80.38deg, #FFD9E5 14.49%, rgba(242, 153, 133, 0.68) 38.03%, rgba(239, 20, 92, 0.38) 61.35%, rgba(171, 24, 184, 0.25) 79.98%, rgba(24, 174, 184, 0.2) 85.51%);
+        height: 1px;
       }
-    }
-    .s-link {
-      color: var(--s-color-theme-accent);
-      padding: 0;
-      &:hover, &:active, &:focus {
-        color: var(--s-color-base-content-primary);
+
+      .icon {
+        width: 50px
       }
-    }
-    .articles {
-      margin-top: $inner-spacing-big;
-    }
-    .article-card {
-      margin: $inner-spacing-mini / 2;
-      background-color: var(--s-color-base-background);
-      border-color: transparent;
-      color: var(--s-color-base-content-secondary);
-      letter-spacing: $s-letter-spacing-medium;
-      font-feature-settings: $s-font-feature-settings-card-body;
       .title {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        line-height: $s-line-height-small;
-        font-feature-settings: $s-font-feature-settings-card-title;
-        font-weight: 600;
-        i {
-          color: var(--s-color-theme-accent);
-          margin-bottom: $inner-spacing-small;
+        font-family: Sora;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 36px;
+        line-height: 100%;
+
+        // display: flex;
+        // align-items: center;
+        letter-spacing: -0.02em;
+        font-feature-settings: 'case' on, 'liga' off;
+
+        color: #D5CDD0;
+
+        .link {
+          width: 12px;
+          margin-left: 5px;
+          margin-bottom: 10px;
         }
       }
     }
   }
-}
-@include large-mobile {
-  .layout {
-    .articles .s-col-sm-6 {
-      width: 50%;
-    }
-    .content .article-card {
-      height: 186px;
-    }
+  &-8 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    position: relative;
+
+    padding-top: 5rem;
   }
 }
-@include tablet {
-  .layout {
-    .banner {
-      height: 256px;
-    }
-    .content {
-      padding-right: $inner-spacing-mini * 4;
-      padding-left: $inner-spacing-mini * 4;
-      .terms, .links {
-        padding-right: 0;
-        padding-left: 0;
-      }
-      .terms .title {
-        font-size: var(--s-heading3-font-size);
-        font-weight: 400;
-        max-width: 75%;
-        width: 100%;
-      }
-      .s-primary + .s-link {
-        margin-left: $inner-spacing-mini * 4;
-      }
-    }
-  }
-}
-@include desktop {
-  .layout {
-    .articles .s-col-sm-6 {
-      width: 25%;
-    }
-    .content {
-      .terms .title {
-        font-size: var(--s-heading1-font-size);
-        line-height: $s-line-height-mini;
-      }
-      .article-card {
-        height: 209px;
-      }
-    }
-  }
-}
-@include large-desktop {
-  .layout {
-    .content {
-      .article-card {
-        height: 186px;
-      }
-    }
-  }
-}
-@include huge-desktop {
-  .layout {
-    .content {
-      .article-card {
-        height: 140px;
-      }
-    }
+
+.text {
+  font-family: Sora;
+  font-style: normal;
+  font-weight: 200;
+  font-size: 12px;
+  line-height: 160%;
+
+  letter-spacing: -0.02em;
+  font-feature-settings: 'case' on, 'liga' off;
+
+  color: #2A171F;
+
+  @media screen and (min-width: 1550px) {
+    font-size: 14px;
   }
 }
 </style>
