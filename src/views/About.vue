9a133cefac273403b4382ae57023c575a476ca74
Pobepto <roobertdantes@gmail.com>
2021-04-19 22:49:07 +0300

    fix: improve style due to comments

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,44 +1,44 @@
 <template>
   <div class="layout">
     <div class="content">
-      <div class="about-1">
-        <img src="@/assets/about/about01.png" draggable="false" class="img unselectable" />
-        <div class="about-1-text">
+      <div class="about-main">
+        <img src="@/assets/about/about01.png" draggable="false" class="main unselectable" />
+        <div class="about-main-text">
           <span class="text">Cross-chain exchange of assets, simple creation and listing of new assets, and the easiest
             swaps ever. The interoperable future of Polkadot is here and we are at the forefront!</span>
         </div>
         <img src="@/assets/about/web3.png" draggable="false" class="web3 unselectable" />
       </div>
-      <div class="about-2">
+      <div class="about-trading">
         <img src="@/assets/about/about02.png" draggable="false" class="unselectable">
-        <div class="about-2-text-1">
+        <div class="about-trading-text-1">
           <span class="text">Polkaswap is a non-custodial, cross-chain AMM DEX protocol for swapping tokens based on
             Polkadot and Kusama relay chains, Polkadot and Kusama parachains, and blockchains directly connected via
             bridges.</span>
         </div>
-        <div class="about-2-text-2">
+        <div class="about-trading-text-2">
           <div class="bubble-block">
             <img src="@/assets/about/about02-1.png" draggable="false" class="unselectable" style="width: 30%">
             <span class="text">Polkaswap removes trusted intermediaries and provides the opportunity for faster
               trading</span>
           </div>
         </div>
-        <div class="about-2-text-3">
+        <div class="about-trading-text-3">
           <div class="bubble-block">
             <img src="@/assets/about/about02-2.png" draggable="false" class="unselectable" style="width: 30%">
             <span class="text">Polkaswap is a community project and devs collaborate on the open source code using the
-              <a href="https://en.wikipedia.org/wiki/BSD_licenses#4-clause_license_(original_%22BSD_License%22)" title="BSD 4-clause license">BSD 4-clause license</a></span>
+              <a href="https://en.wikipedia.org/wiki/BSD_licenses#4-clause_license_(original_%22BSD_License%22)" title="BSD 4-clause license" class="text">BSD 4-clause license</a></span>
           </div>
         </div>
-        <img src="@/assets/about/about02-shadow-1.png" draggable="false" class="unselectable about-2-shadow-1">
+        <img src="@/assets/about/about02-shadow-1.png" draggable="false" class="unselectable about-trading-shadow-1">
       </div>
-      <div class="about-3">
+      <div class="about-liquidity">
         <img src="@/assets/about/about03.png" draggable="false" class="unselectable">
-        <div class="about-3-text-1">
+        <div class="about-liquidity-text-1">
           <span class="text">Polkaswap combines multiple liquidity sources under a common liquidity aggregation
             algorithm, operating completely on-chain, in a trustless and decentralized way.</span>
         </div>
-        <div class="about-3-text-2">
+        <div class="about-liquidity-text-2">
           <div class="bubble-block">
             <img src="@/assets/about/about03-1.png" draggable="false" class="unselectable" style="width: 30%">
             <span class="text">When traders call the swap function, the liquidity aggregation algorithm will fill orders
@@ -46,44 +46,44 @@
               other API-driven sources.</span>
           </div>
         </div>
-        <div class="about-3-text-3">
+        <div class="about-liquidity-text-3">
           <div class="bubble-block">
             <img src="@/assets/about/about03-2.png" draggable="false" class="unselectable" style="width: 30%">
             <span class="text">Because Polkaswap is an open-source project, anyone can add more liquidity sources by
               making contributions to the Polkaswap codebase.</span>
           </div>
         </div>
-        <img src="@/assets/about/about03-shadow-1.png" draggable="false" class="unselectable about-3-shadow-1">
+        <img src="@/assets/about/about03-shadow-1.png" draggable="false" class="unselectable about-liquidity-shadow-1">
       </div>
-      <div class="about-4">
+      <div class="about-swap">
         <img src="@/assets/about/about04.png" draggable="false" class="unselectable">
-        <div class="about-4-text-1">
+        <div class="about-swap-text-1">
           <span class="text">Go beyond the limits of current DEXs by adding tokens from the Polkadot ecosystem as well
             as other blockchains.</span>
         </div>
-        <div class="about-4-text-2">
+        <div class="about-swap-text-2">
           <div class="bubble-block">
             <img src="@/assets/about/about04-1.png" draggable="false" class="unselectable" style="width: 30%">
             <span class="text">Create, list and trade your own tokens on the SORA network.</span>
           </div>
         </div>
-        <div class="about-4-text-3">
+        <div class="about-swap-text-3">
           <div class="bubble-block">
             <img src="@/assets/about/about04-2.png" draggable="false" class="unselectable" style="width: 30%">
             <span class="text">Our core infrastructure uses Parity Substrate, which is more scalable than Ethereum, and
               does not use expensive mining for consensus.</span>
           </div>
         </div>
-        <img src="@/assets/about/about04-shadow-1.png" draggable="false" class="unselectable about-4-shadow-1">
+        <img src="@/assets/about/about04-shadow-1.png" draggable="false" class="unselectable about-swap-shadow-1">
       </div>
-      <div class="about-5">
+      <div class="about-pswap">
         <img src="@/assets/about/about05.png" draggable="false" class="unselectable">
-        <div class="about-5-text-1">
+        <div class="about-pswap-text-1">
           <span class="text">PSWAP was created by community governance by voting on its release. It is a utility and
             governance token used to reward liquidity providers on Polkaswap. Unlike most other reward tokens, PSWAP is
             burned with transactions and decreases in supply over time.</span>
         </div>
-        <div class="about-5-text-2">
+        <div class="about-pswap-text-2">
           <div class="bubble-block">
             <img src="@/assets/about/about05-1.png" draggable="false" class="unselectable" style="width: 30%">
             <span class="text">The 0.3% fee for every swap on the Polkaswap DEX is used to buy back PSWAP tokens, which
@@ -91,28 +91,28 @@
               with time this percentage will decrease to 35% after 4 years.</span>
           </div>
         </div>
-        <img src="@/assets/about/about05-shadow-1.png" draggable="false" class="unselectable about-5-shadow-1">
+        <img src="@/assets/about/about05-shadow-1.png" draggable="false" class="unselectable about-pswap-shadow-1">
       </div>
-      <div class="about-6">
+      <div class="about-video">
         <img src="@/assets/about/about06.png" draggable="false" class="unselectable" style="width: 70%; cursor: pointer;">
-        <img src="@/assets/about/about06-shadow-1.png" draggable="false" class="unselectable about-6-shadow-1">
+        <img src="@/assets/about/about06-shadow-1.png" draggable="false" class="unselectable about-video-shadow-1">
       </div>
-      <div class="about-7">
-        <div class="about-7-part" @click="openLink('https://sora.org/validator')">
+      <div class="about-links">
+        <div class="about-links-part" @click="openLink('https://sora.org/validator')">
           <img src="@/assets/about/about07-1.png" draggable="false" class="unselectable icon">
           <span class="title">Become a SORA Validator<img src="@/assets/about/about07-3.png" class="link"></span>
           <span class="text">Secure the future of the SORA network and decentralized apps like Polkaswap, and earn
             rewards along the way by becoming a SORA validator.</span>
         </div>
-        <div class="about-7-part" @click="openLink('https://sora.org')">
+        <div class="about-links-part" @click="openLink('https://sora.org')">
           <img src="@/assets/about/about07-2.png" draggable="false" class="unselectable icon">
           <span class="title">Connect a Chain to SORA<img src="@/assets/about/about07-3.png" class="link"></span>
           <span class="text">Grow the decentralized world economy by connecting more chains to SORA and Polkaswap using
             the HASHI bridge protocol.</span>
         </div>
-        <img src="@/assets/about/about07-shadow-1.png" draggable="false" class="unselectable about-7-shadow-1">
+        <img src="@/assets/about/about07-shadow-1.png" draggable="false" class="unselectable about-links-shadow-1">
       </div>
-      <div class="about-8">
+      <div class="about-network">
         <img src="@/assets/about/about08.png" draggable="false" class="unselectable" style="width: 65%">
         <span class="text" style="width: 55%; text-align: center;">Polkaswap is built on the SORA Network, focusing on
           interoperability to connect the rest of the crypto ecosystem to Polkadot.</span>
@@ -152,11 +152,6 @@ export default class About extends Mixins(TranslationMixin) {
   user-drag: none;
   user-select: none;
 }
-.img {
-  min-width: 1200px;
-  max-width: 1700px;
-  width: 100%;
-}
 
 .web3 {
   width: 100px;
@@ -176,12 +171,18 @@ export default class About extends Mixins(TranslationMixin) {
 
 .about {
   margin: auto;
-  &-1 {
+  &-main {
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
     position: relative;
+
+    .main {
+      min-width: 800px;
+      max-width: 1700px;
+      width: 100%;
+    }
 
     &-text {
       top: 60%;
@@ -191,7 +192,7 @@ export default class About extends Mixins(TranslationMixin) {
       text-align: center;
     }
   }
-  &-2 {
+  &-trading {
     min-width: 800px;
     padding-top: 5rem;
     display: flex;
@@ -230,7 +231,7 @@ export default class About extends Mixins(TranslationMixin) {
       width: 100%;
     }
   }
-  &-3 {
+  &-liquidity {
     min-width: 800px;
     display: flex;
     justify-content: center;
@@ -267,8 +268,19 @@ export default class About extends Mixins(TranslationMixin) {
       z-index: -1;
       width: 100%;
     }
+    @media screen and (max-width: 1024px) {
+      &-text-1 {
+        left: 6.5%;
+      }
+      &-text-2 {
+        left: 2%;
+      }
+      &-text-3 {
+        left: 2%;
+      }
+    }
   }
-  &-4 {
+  &-swap {
     min-width: 800px;
     display: flex;
     justify-content: center;
@@ -286,14 +298,14 @@ export default class About extends Mixins(TranslationMixin) {
       position: absolute;
     }
     &-text-2 {
-      top: 27%;
+      top: 28%;
       right: 5.5%;
       width: 45%;
 
       position: absolute;
     }
     &-text-3 {
-      top: 52%;
+      top: 48%;
       right: 5.5%;
       width: 45%;
 
@@ -306,7 +318,7 @@ export default class About extends Mixins(TranslationMixin) {
       width: 100%;
     }
   }
-  &-5 {
+  &-pswap {
     min-width: 800px;
     display: flex;
     justify-content: flex-start;
@@ -324,7 +336,7 @@ export default class About extends Mixins(TranslationMixin) {
       position: absolute;
     }
     &-text-2 {
-      top: 55%;
+      top: 58%;
       left: 32%;
       width: 50%;
 
@@ -337,7 +349,7 @@ export default class About extends Mixins(TranslationMixin) {
       width: 100%;
     }
   }
-  &-6 {
+  &-video {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -349,9 +361,13 @@ export default class About extends Mixins(TranslationMixin) {
       top: -100%;
       z-index: -1;
       width: 120%;
+
+      @media screen and (max-width: 1200px) {
+        width: 100%;
+      }
     }
   }
-  &-7 {
+  &-links {
     width: 70%;
     max-width: 1600px;
     padding-top: 7rem;
@@ -404,7 +420,7 @@ export default class About extends Mixins(TranslationMixin) {
       width: 100%;
     }
   }
-  &-8 {
+  &-network {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -431,7 +447,7 @@ export default class About extends Mixins(TranslationMixin) {
     font-size: 14px;
   }
   @media screen and (min-width: 1250px) {
-    font-size: 16px;
+    font-size: 15px;
   }
   @media screen and (min-width: 1800px) {
     font-size: 18px;
