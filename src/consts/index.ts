e6fdf22e98d1f8dec900d729dfac5e4750372671
Stefan Popov <stefanpopov@list.ru>
2021-05-18 14:35:17 +0400

    Recalc eth fee

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -229,11 +229,12 @@ export enum NetworkTypes {
 export const EthSymbol = 'ETH'
 
 const gasLimit = {
-  approve: 66000 * 2,
-  sendERC20ToSidechain: 81000 * 2,
-  mintTokensByPeers: 191285 * 2,
-  receiveByEthereumAssetAddress: 253979 * 2,
-  receiveBySidechainAssetId: 252659 * 2
+  approve: 69687,
+  sendERC20ToSidechain: 85474,
+  sendEthToSidechain: 39087,
+  mintTokensByPeers: 254627,
+  receiveByEthereumAssetAddress: 247274,
+  receiveBySidechainAssetId: 252659
 }
 /**
  * It's in gwei.
@@ -246,6 +247,7 @@ export const EthereumGasLimits = [
     XOR: gasLimit.approve + gasLimit.sendERC20ToSidechain,
     VAL: gasLimit.approve + gasLimit.sendERC20ToSidechain,
     PSWAP: gasLimit.approve + gasLimit.sendERC20ToSidechain,
+    ETH: gasLimit.sendEthToSidechain,
     [KnownBridgeAsset.Other]: gasLimit.approve + gasLimit.sendERC20ToSidechain
   },
   // SORA -> ETH
@@ -253,6 +255,7 @@ export const EthereumGasLimits = [
     XOR: gasLimit.mintTokensByPeers,
     VAL: gasLimit.mintTokensByPeers,
     PSWAP: gasLimit.receiveBySidechainAssetId,
+    ETH: gasLimit.receiveByEthereumAssetAddress,
     [KnownBridgeAsset.Other]: gasLimit.receiveByEthereumAssetAddress
   }
 ]
