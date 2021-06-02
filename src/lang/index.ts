a4cc059777a4fd65c5213c6df6031049cc093973
Stefan Popov <stefanpopov@list.ru>
2021-06-02 13:25:47 +0400

    Add script for i18n values generation

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -1,17 +1,18 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
-import { en as walletEn } from '@soramitsu/soraneo-wallet-web'
 
-import en from './en'
+import { Language } from '@/consts'
+
+import en from './en.json'
 
 Vue.use(VueI18n)
 
 const messages = {
-  en: { ...walletEn, ...en }
+  en
 }
 
 const i18n = new VueI18n({
-  locale: 'en',
+  locale: Language.EN,
   messages
 })
 
