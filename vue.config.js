5bfc83ef8890c8e573358f07d5416f0190a4eab0
Stefan Popov <stefanpopov@list.ru>
2021-06-17 02:01:49 +0400

    Check

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,21 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
 module.exports = {
   publicPath: './',
-  configureWebpack: config => {
-    // prepare icons content to unicode
-    config.module.rules.filter(rule => {
-      return rule.test.toString().indexOf('scss') !== -1
-    })
-      .forEach(rule => {
-        rule.oneOf.forEach(oneOfRule => {
-          oneOfRule.use.splice(oneOfRule.use.indexOf(require.resolve('sass-loader')), 0,
-            { loader: require.resolve('css-unicode-loader') })
-        })
-      })
+  configureWebpack: {
+    plugins: [new BundleAnalyzerPlugin()]
   },
+  // configureWebpack: config => {
+  //   // prepare icons content to unicode
+  //   config.module.rules.filter(rule => {
+  //     return rule.test.toString().indexOf('scss') !== -1
+  //   }).forEach(rule => {
+  //     rule.oneOf.forEach(oneOfRule => {
+  //       oneOfRule.use.splice(oneOfRule.use.indexOf(require.resolve('sass-loader')), 0,
+  //         { loader: require.resolve('css-unicode-loader') })
+  //     })
+  //   })
+  // },
   css: {
     loaderOptions: {
       sass: {
