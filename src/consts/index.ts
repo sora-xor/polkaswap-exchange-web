29bc7688b1bb69673211c2834794e5f1dd0c4ea9
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2021-06-22 12:42:47 +0300

    add TokenAddress component

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -107,7 +107,8 @@ export enum Components {
   TokensRow = 'Rewards/TokensRow',
   RewardsAmountHeader = 'Rewards/AmountHeader',
   RewardsAmountTable = 'Rewards/AmountTable',
-  TokenSelectButton = 'Input/TokenSelectButton'
+  TokenSelectButton = 'Input/TokenSelectButton',
+  TokenAddress = 'Input/TokenAddress'
 }
 
 interface SidebarMenuItem {
--- src/views/Bridge.vue
+++ src/views/Bridge.vue
@@ -6,6 +6,7 @@
         class="bridge-content"
         border-radius="medium"
         shadow="always"
+        size="big"
         primary
       >
         <generic-page-header class="header--bridge" :title="t('bridge.title')" :tooltip="t('bridge.info')">
@@ -59,6 +60,9 @@
             </s-button>
             <token-select-button class="el-button--select-token" icon="chevron-down-rounded-16" :token="asset" @click="openSelectAssetDialog" />
           </div>
+          <div slot="bottom" class="input-line input-line--footer">
+            <token-address v-if="isAssetSelected" v-bind="asset" :external="!isSoraToEvm" />
+          </div>
         </s-float-input>
 
         <s-button class="s-button--switch" type="action" icon="arrows-swap-90-24" @click="handleSwitchItems" />
@@ -88,6 +92,9 @@
             <token-select-button class="el-button--select-token" :token="asset" />
           </div>
           <template #bottom>
+            <div class="input-line input-line--footer">
+              <token-address v-if="isAssetSelected" v-bind="asset" :external="isSoraToEvm" />
+            </div>
             <div v-if="isNetworkBConnected && isSoraToEvm" class="bridge-item-footer">
               <s-divider />
               <toggle-text-button
@@ -190,7 +197,8 @@ import {
   getAssetBalance,
   findAssetInCollection,
   asZeroValue,
-  isEthereumAddress
+  isEthereumAddress,
+  formatAddress
 } from '@/utils'
 import { bridgeApi } from '@/utils/bridge'
 
@@ -205,7 +213,8 @@ const namespace = 'bridge'
     SelectRegisteredAsset: lazyComponent(Components.SelectRegisteredAsset),
     ConfirmBridgeTransactionDialog: lazyComponent(Components.ConfirmBridgeTransactionDialog),
     ToggleTextButton: lazyComponent(Components.ToggleTextButton),
-    TokenSelectButton: lazyComponent(Components.TokenSelectButton)
+    TokenSelectButton: lazyComponent(Components.TokenSelectButton),
+    TokenAddress: lazyComponent(Components.TokenAddress)
   }
 })
 export default class Bridge extends Mixins(
@@ -529,7 +538,7 @@ $bridge-input-color: var(--s-color-base-content-tertiary);
       width: 100%;
     }
     &--next {
-      margin-top: $inner-spacing-mini;
+      margin-top: $inner-spacing-medium;
       width: 100%;
     }
   }
