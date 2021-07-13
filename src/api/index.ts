f036ec37e837782562d776dbd6aa310345762a40
Nikita Polyakov <53777036+Nikita-Polyakov@users.noreply.github.com>
2021-07-13 10:25:35 +0300

    update axios baseUrl (#365)

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,25 @@
 import axiosInstance from 'axios'
+import VueRouter from 'vue-router'
 
 export const BASE_URL = process.env.BASE_URL
 
 const axios = axiosInstance.create()
-axios.defaults.baseURL = BASE_URL
+
+const detectBaseUrl = (router?: VueRouter): string => {
+  if (BASE_URL) return BASE_URL
+
+  if (router && router.mode === 'hash') {
+    const { origin, pathname } = window.location
+
+    return `${origin}${pathname}`
+  }
+
+  return ''
+}
+
+export const updateBaseUrl = (router: VueRouter): void => {
+  const baseUrl = detectBaseUrl(router)
+  axios.defaults.baseURL = baseUrl
+}
 
 export default axios
