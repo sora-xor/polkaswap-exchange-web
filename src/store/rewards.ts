968b49cbb7036ecd4424447e5ab8a8c3156e08ca
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2021-07-20 14:17:26 +0300

    fix ethers signature creating

--- src/store/rewards.ts
+++ src/store/rewards.ts
@@ -227,9 +227,8 @@ const actions = {
       if (externalRewardsSelected && state.transactionStep === 1) {
         const ethersInstance = await ethersUtil.getEthersInstance()
         const internalAddressHex = await ethersUtil.accountAddressToHex(internalAddress)
-
         const message = ethers.utils.keccak256(internalAddressHex)
-        const signature = ethersInstance.getSigner().signMessage(message)
+        const signature = await ethersInstance.getSigner().signMessage(message)
 
         commit(types.SET_SIGNATURE, signature)
         commit(types.SET_TRANSACTION_STEP, 2)
@@ -250,6 +249,7 @@ const actions = {
         }
       }
     } catch (error) {
+      console.log(error)
       commit(types.SET_TRANSACTION_ERROR, true)
       commit(types.SET_REWARDS_CLAIMING, false)
       throw error
