b8aa267fb7e16e1ea99c81c5b7dbecb3c563e1d8
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2021-08-04 15:02:53 +0300

    update rewards view

--- src/views/Rewards.vue
+++ src/views/Rewards.vue
@@ -3,7 +3,7 @@
     <generic-page-header :title="t('rewards.title')" />
     <div class="rewards-content" v-loading="!parentLoading && loading">
       <gradient-box class="rewards-block" :symbol="gradientSymbol">
-        <div class="rewards-box">
+        <div :class="rewardsBoxClasses">
           <tokens-row :symbols="rewardTokenSymbols" />
           <div v-if="claimingInProgressOrFinished" class="rewards-claiming-text">
             {{ claimingStatusMessage }}
@@ -79,6 +79,7 @@ import { Component, Mixins } from 'vue-property-decorator'
 import { Action, Getter, State } from 'vuex-class'
 import { AccountAsset, KnownAssets, KnownSymbols, RewardInfo, RewardsInfo, CodecString, FPNumber } from '@sora-substrate/util'
 import { FormattedAmountMixin } from '@soramitsu/soraneo-wallet-web'
+import Theme from '@soramitsu/soramitsu-js-ui/lib/types/Theme'
 
 import ethersUtil from '@/utils/ethers-util'
 import { lazyComponent } from '@/router'
@@ -116,6 +117,7 @@ export default class Rewards extends Mixins(FormattedAmountMixin, WalletConnectM
   @State(state => state.rewards.selectedInternalRewards) selectedInternalRewards!: Array<RewardInfo>
   @State(state => state.rewards.selectedExternalRewards) selectedExternalRewards!: Array<RewardInfo>
 
+  @Getter libraryTheme!: Theme
   @Getter('tokenXOR', { namespace: 'assets' }) tokenXOR!: AccountAsset
   @Getter('rewardsAvailable', { namespace: 'rewards' }) rewardsAvailable!: boolean
   @Getter('externalRewardsAvailable', { namespace: 'rewards' }) externalRewardsAvailable!: boolean
@@ -302,6 +304,17 @@ export default class Rewards extends Mixins(FormattedAmountMixin, WalletConnectM
     return this.rewardsClaiming || (this.rewardsAvailable && this.isInsufficientBalance)
   }
 
+  get rewardsBoxClasses (): Array<string> {
+    const base = 'rewards-box'
+    const classes = [base]
+
+    if (this.libraryTheme === Theme.DARK) {
+      classes.push(`${base}--dark`)
+    }
+
+    return classes
+  }
+
   async handleAction (): Promise<void> {
     if (!this.isSoraAccountConnected) {
       return this.connectInternalWallet()
@@ -374,14 +387,6 @@ export default class Rewards extends Mixins(FormattedAmountMixin, WalletConnectM
 .container.rewards .el-loading-mask {
   border-radius: var(--s-border-radius-small);
 }
-.rewards-connect-button.el-button.neumorphic {
-  &, &:hover, &.focusing {
-    background: transparent;
-    color: var(--s-color-base-on-accent);
-    border: 1px solid var(--s-color-base-on-accent);
-    box-shadow: none;
-  }
-}
 .rewards-action-button i {
   top: $inner-spacing-mini;
 }
@@ -405,7 +410,11 @@ $hint-font-size: 13px;
     display: flex;
     flex-flow: column nowrap;
     align-items: center;
-    color: var(--s-color-base-on-accent); // TODO: use color variable from ui library
+    color: var(--s-color-base-on-accent);
+
+    &--dark {
+      color: var(--s-color-base-content-primary);
+    }
 
     & > *:not(:last-child) {
       margin-bottom: $inner-spacing-mini;
