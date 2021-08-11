067eb11b6838a090910d9e7cb3ccd5d8e056ff96
Nikita Polyakov <53777036+Nikita-Polyakov@users.noreply.github.com>
2021-08-11 12:17:46 +0300

    fix ipv4 regexp (#425)

--- src/utils/regexp.ts
+++ src/utils/regexp.ts
@@ -2,7 +2,7 @@ const ws = 'wss?:\\/\\/'
 const port = '(?::\\d{1,5})'
 const dns = '(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]'
 const ipv4part = '(?:25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d)'
-const ipv4 = `^${ipv4part}(?:\\.${ipv4part}){3}$`
+const ipv4 = `${ipv4part}(?:\\.${ipv4part}){3}`
 
 const exactStart = exp => `^${exp}`
 const exact = exp => `^${exp}$`
