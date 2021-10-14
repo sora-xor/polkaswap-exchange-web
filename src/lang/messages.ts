4993b6ffc55989dcb961ec6fc5490c9e6d4cfeb8
Nikita Polyakov <53777036+Nikita-Polyakov@users.noreply.github.com>
2021-10-14 12:31:04 +0300

    Fix/wallet translations (#486)
    * first update
    
    * update Transfer Operation translations

--- src/lang/messages.ts
+++ src/lang/messages.ts
@@ -181,7 +181,7 @@ export default {
     [Operation.ClaimRewards]: 'Claim Rewards',
     andText: 'and',
     [TransactionStatus.Finalized]: {
-      [Operation.Transfer]: 'Sent {amount} {symbol} to {address}',
+      [Operation.Transfer]: '{action} {amount} {symbol} {direction} {address}',
       [Operation.Swap]: 'Swapped {amount} {symbol} for {amount2} {symbol2}',
       [Operation.AddLiquidity]: 'Supplied {amount} {symbol} and {amount2} {symbol2}',
       [Operation.RemoveLiquidity]: 'Removed {amount} {symbol} and {amount2} {symbol2}',
