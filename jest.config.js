51e446c66eb8a3e6ec0f3f45c883c0cd053082d3
RustemYuzlibaev <rustem.yuzlibaev@yandex.ru>
2021-11-18 09:47:23 +0300

    Setup unit tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  setupFiles: ['./tests/unit/setup.js'],
+  setupFiles: ['./tests/unit/setup.js', '<rootDir>/.jest/register-context.js'],
   verbose: true,
   transform: {
     '^.+\\.js$': 'babel-jest',
