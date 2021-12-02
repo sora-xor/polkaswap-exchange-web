fa09575d5a6a096ffb6ee8d998397003eeb4e1eb
Rustem <rustem.yuzlibaev@yandex.ru>
2021-12-02 10:01:43 +0300

    add account button unit-test (#523)
    * add account button unit-test
    
    * Update SwapInfo.spec.ts.snap

---
+++ tests/mocks/account.ts
@@ -0,0 +1,7 @@
+import { Account } from '@soramitsu/soraneo-wallet-web/lib/types/common';
+
+export const MOCK_ACCOUNT: Account = {
+  address: 'cnRXua6zs8TaE87BQFL6uWVbT2g6GXsUjwk6PTvL6UHcHDCvo',
+  name: 'Sora Name',
+  isExternal: true,
+};
