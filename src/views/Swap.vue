49a52843da1a89effad4e2357593a8af04248bc4
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2021-12-10 10:23:45 +0300

    update subscription

--- src/views/Swap.vue
+++ src/views/Swap.vue
@@ -9,7 +9,7 @@
             class="el-button--settings"
             type="action"
             icon="basic-settings-24"
-            :disabled="!pairLiquiditySourcesAvailable"
+            :disabled="!marketAlgorithmsAvailable"
             @click="openSettingsDialog"
           />
         </template>
@@ -208,6 +208,7 @@ import type {
   NetworkFeesObject,
   QuotePaths,
   QuotePayload,
+  PrimaryMarketsEnabledAssets,
 } from '@sora-substrate/util';
 
 import TranslationMixin from '@/components/mixins/TranslationMixin';
@@ -260,7 +261,7 @@ export default class Swap extends Mixins(mixins.FormattedAmountMixin, Translatio
   @Getter('tokenTo', { namespace }) tokenTo!: AccountAsset;
   @Getter('isAvailable', { namespace }) isAvailable!: boolean;
   @Getter('swapLiquiditySource', { namespace }) liquiditySource!: LiquiditySourceTypes;
-  @Getter('pairLiquiditySourcesAvailable', { namespace }) pairLiquiditySourcesAvailable!: boolean;
+  @Getter('marketAlgorithmsAvailable', { namespace }) marketAlgorithmsAvailable!: boolean;
   @Getter('swapMarketAlgorithm', { namespace }) swapMarketAlgorithm!: string;
 
   @Action('setTokenFromAddress', { namespace }) setTokenFromAddress!: (address?: string) => Promise<void>;
@@ -272,6 +273,10 @@ export default class Swap extends Mixins(mixins.FormattedAmountMixin, Translatio
   @Action('setLiquidityProviderFee', { namespace }) setLiquidityProviderFee!: (value: CodecString) => Promise<void>;
   @Action('reset', { namespace }) reset!: AsyncVoidFn;
   @Action('getAssets', { namespace: 'assets' }) getAssets!: AsyncVoidFn;
+
+  @Action('setPrimaryMarketsEnabledAssets', { namespace }) setPrimaryMarketsEnabledAssets!: (
+    assets: PrimaryMarketsEnabledAssets
+  ) => Promise<void>;
 
   @Action('setRewards', { namespace }) setRewards!: (rewards: Array<LPRewardsInfo>) => Promise<void>;
   @Action('setSubscriptionPayload', { namespace }) setSubscriptionPayload!: (payload: QuotePayload) => Promise<void>;
@@ -294,9 +299,11 @@ export default class Swap extends Mixins(mixins.FormattedAmountMixin, Translatio
   private updateConnectionSubsriptions(nodeConnected: boolean) {
     if (nodeConnected) {
       this.updateSubscriptions();
+      this.subscribeOnEnabledAssets();
       this.subscribeOnSwapReserves();
     } else {
       this.resetSubscriptions();
+      this.cleanEnabledAssetsSubscription();
       this.cleanSwapReservesSubscription();
     }
   }
@@ -308,6 +315,7 @@ export default class Swap extends Mixins(mixins.FormattedAmountMixin, Translatio
   showSelectTokenDialog = false;
   showConfirmSwapDialog = false;
   liquidityReservesSubscription: Nullable<Subscription> = null;
+  enabledAssetsSubscription: Nullable<Subscription> = null;
   recountSwapValues = debouncedInputHandler(this.runRecountSwapValues, 100);
 
   get areTokensSelected(): boolean {
@@ -442,6 +450,10 @@ export default class Swap extends Mixins(mixins.FormattedAmountMixin, Translatio
         await this.setTokenFromAddress(XOR.address);
         await this.setTokenToAddress();
       }
+
+      if (!this.enabledAssetsSubscription) {
+        this.subscribeOnEnabledAssets();
+      }
     });
   }
 
@@ -512,6 +524,22 @@ export default class Swap extends Mixins(mixins.FormattedAmountMixin, Translatio
       console.error(error);
       resetOppositeValue();
     }
+  }
+
+  private cleanEnabledAssetsSubscription(): void {
+    if (!this.enabledAssetsSubscription) {
+      return;
+    }
+    this.enabledAssetsSubscription.unsubscribe();
+    this.enabledAssetsSubscription = null;
+  }
+
+  private subscribeOnEnabledAssets(): void {
+    this.cleanEnabledAssetsSubscription();
+
+    this.enabledAssetsSubscription = api
+      .subscribeOnPrimaryMarketsEnabledAssets()
+      .subscribe(this.setPrimaryMarketsEnabledAssets);
   }
 
   private cleanSwapReservesSubscription(): void {
@@ -617,6 +645,7 @@ export default class Swap extends Mixins(mixins.FormattedAmountMixin, Translatio
 
   destroyed(): void {
     this.reset();
+    this.cleanEnabledAssetsSubscription();
     this.cleanSwapReservesSubscription();
   }
 }
