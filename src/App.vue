77f4ad1a120eef1508f0029fe7a44c42813d4da1
Stefan Popov <stefanpopov@list.ru>
2021-12-17 12:27:26 +0400

    Revert "Update wallet"
    This reverts commit 26ae511c0a0c1a55c30b3dabf243832ed502453a.

--- src/App.vue
+++ src/App.vue
@@ -57,7 +57,6 @@ export default class App extends Mixins(mixins.TransactionMixin, NodeErrorMixin)
   @Action resetAccountAssetsSubscription!: AsyncVoidFn;
   @Action trackActiveTransactions!: AsyncVoidFn;
   @Action resetActiveTransactions!: AsyncVoidFn;
-  @Action resetRuntimeVersionSubscription!: AsyncVoidFn;
   @Action resetFiatPriceAndApySubscription!: AsyncVoidFn;
 
   @Action updateAccountAssets!: AsyncVoidFn;
@@ -122,7 +121,6 @@ export default class App extends Mixins(mixins.TransactionMixin, NodeErrorMixin)
     await this.resetFiatPriceAndApySubscription();
     await this.resetActiveTransactions();
     await this.resetAccountAssetsSubscription();
-    await this.resetRuntimeVersionSubscription();
     await connection.close();
   }
 
