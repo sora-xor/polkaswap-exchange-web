38ab336e84009854990bc0bac57ee2100c4c7f39
Rustem <rustem.yuzlibaev@yandex.ru>
2022-02-22 22:20:47 +0500

    nft adjustments (#604)
    * update wallet
    
    * update translations
    
    * Add api key
    
    * Update wallet version
    
    * Update token icons
    
    Co-authored-by: Stefan Popov <stefanpopov@list.ru>

--- src/components/TokenLogo.vue
+++ src/components/TokenLogo.vue
@@ -5,35 +5,54 @@
 <script lang="ts">
 import { Component, Mixins, Prop } from 'vue-property-decorator';
 import { Getter } from 'vuex-class';
-import type { Asset, AccountAsset, Whitelist } from '@sora-substrate/util/build/assets/types';
+import { api } from '@soramitsu/soraneo-wallet-web';
+import type { Asset, AccountAsset, Whitelist, WhitelistItem } from '@sora-substrate/util/build/assets/types';
 
 import TranslationMixin from '@/components/mixins/TranslationMixin';
 import { LogoSize, ObjectInit } from '@/consts';
 
+// TODO: move to js lib
+type WhitelistIdsBySymbol = {
+  [key: string]: string;
+};
+
 @Component
 export default class TokenLogo extends Mixins(TranslationMixin) {
   @Getter whitelist!: Whitelist;
-  @Getter whitelistIdsBySymbol!: any;
+  @Getter whitelistIdsBySymbol!: WhitelistIdsBySymbol;
 
   @Prop({ type: String, default: '' }) readonly tokenSymbol!: string;
   @Prop({ type: Object, default: ObjectInit }) readonly token!: AccountAsset | Asset;
   @Prop({ type: String, default: LogoSize.MEDIUM, required: false }) readonly size!: LogoSize;
 
-  get tokenClasses(): string {
+  get assetAddress(): string {
+    return this.tokenSymbol ? this.whitelistIdsBySymbol[this.tokenSymbol] : this.token.address;
+  }
+
+  get whitelistedItem(): Nullable<WhitelistItem> {
+    if (!(this.token || this.tokenSymbol)) {
+      return null;
+    }
+    const address = this.assetAddress;
+    return this.whitelist[address];
+  }
+
+  get tokenClasses(): Array<string> {
     const tokenLogoClass = 'token-logo';
     const classes = [tokenLogoClass];
-
     classes.push(`${tokenLogoClass}--${this.size.toLowerCase()}`);
-    return classes.join(' ');
+
+    const asset = this.whitelistedItem;
+    if (!asset) {
+      const isNft = this.token ? api.assets.isNft(this.token) : false;
+      return [...classes, isNft ? 'token-logo-nft' : 's-icon-notifications-info-24'];
+    }
+
+    return classes;
   }
 
   get tokenStyles(): object {
-    if (!(this.token || this.tokenSymbol)) return {};
-
-    const address = this.tokenSymbol ? this.whitelistIdsBySymbol[this.tokenSymbol] : this.token.address;
-    if (!address) return {};
-
-    const asset = this.whitelist[address];
+    const asset = this.whitelistedItem;
     if (asset) {
       return {
         'background-size': '100%',
@@ -48,24 +67,92 @@ export default class TokenLogo extends Mixins(TranslationMixin) {
 
 <style lang="scss" scoped>
 $token-background-color: var(--s-color-base-on-accent);
-// TODO: replace background color with the next one after icon color fix
-// $token-background-color: var(--s-color-base-content-tertiary);
-$default-logo: url('~@/assets/img/token-logo-default.svg');
+$token-color: var(--s-color-base-content-tertiary);
 
 .token-logo {
   background-color: $token-background-color;
-  background-image: $default-logo;
   background-size: 60%;
   background-repeat: no-repeat;
   background-position: 50%;
   border: none;
   box-shadow: none;
   border-radius: 50%;
+  text-align: center;
+  position: relative;
+  font-style: unset;
+  &::before {
+    display: block;
+    color: $token-color;
+  }
+  &-nft::before {
+    font-weight: 800;
+    content: 'NFT';
+  }
 }
 
-@include element-size('token-logo--mini', 16px);
-@include element-size('token-logo--small', 24px);
-@include element-size('token-logo--medium', 32px);
-@include element-size('token-logo--big', var(--s-size-medium));
-@include element-size('token-logo--large', 80px);
+@mixin token-logo-size($size: '') {
+  $className: 'token-logo';
+  @if ($size == 'mini') {
+    $size-px: 16px;
+    $classNameMini: '#{$className}--mini';
+    @include element-size($classNameMini, $size-px);
+    .#{$classNameMini} {
+      line-height: $size-px;
+      font-size: 12px;
+      &.#{$className}-nft::before {
+        font-size: 6px;
+      }
+    }
+  } @else if ($size == 'small') {
+    $size-px: 24px;
+    $classNameSmall: '#{$className}--small';
+    @include element-size($classNameSmall, $size-px);
+    .#{$classNameSmall} {
+      line-height: $size-px;
+      font-size: 18px;
+      &.#{$className}-nft::before {
+        font-size: 8px;
+      }
+    }
+  } @else if ($size == 'medium') {
+    $size-px: 32px;
+    $classNameMedium: '#{$className}--medium';
+    @include element-size($classNameMedium, $size-px);
+    .#{$classNameMedium} {
+      line-height: $size-px;
+      font-size: 24px;
+      &.#{$className}-nft::before {
+        font-size: 12px;
+      }
+    }
+  } @else if ($size == 'big') {
+    $size-px: var(--s-size-medium);
+    $classNameBig: '#{$className}--big';
+    @include element-size($classNameBig, $size-px);
+    .#{$classNameBig} {
+      line-height: $size-px;
+      font-size: 32px;
+      &.#{$className}-nft::before {
+        font-size: 14px;
+      }
+    }
+  } @else if ($size == 'large') {
+    $size-px: 80px;
+    $classNameLarge: '#{$className}--large';
+    @include element-size($classNameLarge, $size-px);
+    .#{$classNameLarge} {
+      line-height: $size-px;
+      font-size: 64px;
+      &.#{$className}-nft::before {
+        font-size: 28px;
+      }
+    }
+  }
+}
+
+@include token-logo-size('mini');
+@include token-logo-size('small');
+@include token-logo-size('medium');
+@include token-logo-size('big');
+@include token-logo-size('large');
 </style>
