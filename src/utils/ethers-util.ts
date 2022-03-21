dda755871a452b82f3d133c9ab17d2517111c08f
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2022-03-21 17:04:54 +0300

    add addToken function

--- src/utils/ethers-util.ts
+++ src/utils/ethers-util.ts
@@ -268,6 +268,34 @@ async function watchEthereum(cb: {
   };
 }
 
+async function addToken(address: string, symbol: string, decimals: number, image?: string) {
+  const ethereum = (window as any).ethereum;
+
+  try {
+    // wasAdded is a boolean. Like any RPC method, an error may be thrown.
+    const wasAdded = await ethereum.request({
+      method: 'wallet_watchAsset',
+      params: {
+        type: 'ERC20', // Initially only supports ERC20, but eventually more!
+        options: {
+          address, // The address that the token is at.
+          symbol, // A ticker symbol or shorthand, up to 5 chars.
+          decimals, // The number of decimals in the token
+          image, // A string url of the token logo
+        },
+      },
+    });
+
+    if (wasAdded) {
+      console.log('Thanks for your interest!');
+    } else {
+      console.log('Your loss!');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 function storeEvmUserAddress(address: string): void {
   storage.set('evmAddress', address);
 }
@@ -386,4 +414,5 @@ export default {
   getEvmTransaction,
   getEvmTransactionReceipt,
   getBlock,
+  addToken,
 };
