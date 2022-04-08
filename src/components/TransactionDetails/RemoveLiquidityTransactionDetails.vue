22ee5a36098efac6977ee0e518d984038f91e341
Stefan Popov <stefanpopov@list.ru>
2022-04-08 02:49:32 +0200

    Update RemoveLiquidityTransactionDetails.vue

--- src/components/TransactionDetails/RemoveLiquidityTransactionDetails.vue
+++ src/components/TransactionDetails/RemoveLiquidityTransactionDetails.vue
@@ -7,23 +7,19 @@
       :value="`${shareOfPool}%`"
     />
     <info-line
-      v-if="price || priceReversed"
+      v-if="priceReversed"
       :label="t('removeLiquidity.price')"
-      :value="`1 ${firstToken.symbol} = ${formatStringValue(priceReversed)}`"
-      :asset-symbol="secondToken.symbol"
+      :value="`1 ${firstTokenSymbol} = ${formattedPriceReversed}`"
+      :asset-symbol="secondTokenSymbol"
     />
-    <info-line
-      v-if="price || priceReversed"
-      :value="`1 ${secondToken.symbol} = ${formatStringValue(price)}`"
-      :asset-symbol="firstToken.symbol"
-    />
+    <info-line v-if="price" :value="`1 ${secondTokenSymbol} = ${formattedPrice}`" :asset-symbol="firstTokenSymbol" />
     <info-line
       v-if="networkFee"
       :label="t('createPair.networkFee')"
       :label-tooltip="t('networkFeeTooltipText')"
       :value="formattedFee"
       :asset-symbol="XOR_SYMBOL"
-      :fiat-value="getFiatAmountByCodecString(networkFee)"
+      :fiat-value="formattedFeeFiatValue"
       is-formatted
     />
   </transaction-details>
@@ -58,12 +54,32 @@ export default class RemoveLiquidityTransactionDetails extends Mixins(mixins.For
 
   @Prop({ default: true, type: Boolean }) readonly infoOnly!: boolean;
 
+  get firstTokenSymbol(): string {
+    return this.firstToken.symbol;
+  }
+
+  get secondTokenSymbol(): string {
+    return this.secondToken.symbol;
+  }
+
+  get formattedPrice(): string {
+    return this.formatStringValue(this.price);
+  }
+
+  get formattedPriceReversed(): string {
+    return this.formatStringValue(this.priceReversed);
+  }
+
   get networkFee(): CodecString {
     return this.networkFees[Operation.RemoveLiquidity];
   }
 
   get formattedFee(): string {
     return this.formatCodecNumber(this.networkFee);
+  }
+
+  get formattedFeeFiatValue(): Nullable<string> {
+    return this.getFiatAmountByCodecString(this.networkFee);
   }
 }
 </script>
