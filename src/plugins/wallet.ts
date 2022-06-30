72f192b01cd8e5bfb8f8c16ecd3981774e3e1a17
Stefan Popov <stefanpopov@list.ru>
2022-06-30 12:53:08 +0400

    Fix issues

--- src/plugins/wallet.ts
+++ src/plugins/wallet.ts
@@ -3,6 +3,6 @@ import Wallet from '@soramitsu/soraneo-wallet-web';
 
 import store from '@/store';
 
-import '@soramitsu/soraneo-wallet-web/lib/soraneo-wallet-web.umd.css';
+import '@soramitsu/soraneo-wallet-web/lib/soraneo-wallet-web.css';
 
 Vue.use(Wallet, { store });
