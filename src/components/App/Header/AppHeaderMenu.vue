d0b7cae1daa53ca458a0e1e87a12decd1036c807
RustemYuzlibaev <rustem.yuzlibaev@yandex.ru>
2022-08-17 13:48:08 +0500

    add checks

--- src/components/App/Header/AppHeaderMenu.vue
+++ src/components/App/Header/AppHeaderMenu.vue
@@ -28,7 +28,7 @@
             {{ text }}
           </s-dropdown-item>
           <div
-            v-if="!notificationActivated"
+            v-if="!notificationActivated && isBrowserNotificationApiAvailable"
             @click="openNotificationDialog"
             class="notif-option el-dropdown-menu__item header-menu__item"
           >
@@ -50,7 +50,7 @@
         <s-icon :name="icon" :size="iconSize" />
       </s-button>
       <s-button
-        v-if="!notificationActivated"
+        v-if="!notificationActivated && isBrowserNotificationApiAvailable"
         type="action"
         :tooltip="t('browserNotificationDialog.button')"
         @click="openNotificationDialog"
@@ -96,6 +96,7 @@ export default class AppHeaderMenu extends Mixins(TranslationMixin) {
   readonly HeaderMenuType = HeaderMenuType;
 
   @state.wallet.settings.shouldBalanceBeHidden private shouldBalanceBeHidden!: boolean;
+  @state.settings.isBrowserNotificationApiAvailable isBrowserNotificationApiAvailable!: boolean;
   @getter.libraryTheme private libraryTheme!: Theme;
   @getter.settings.notificationActivated notificationActivated!: boolean;
 
@@ -184,10 +185,12 @@ export default class AppHeaderMenu extends Mixins(TranslationMixin) {
   }
 
   openNotificationDialog(): void {
-    if (Notification.permission === 'denied') {
-      this.setBrowserNotifsPopupBlocked(true);
-    } else if (Notification.permission === 'default') {
-      this.setBrowserNotifsPopupEnabled(true);
+    if (this.isBrowserNotificationApiAvailable) {
+      if (Notification.permission === 'denied') {
+        this.setBrowserNotifsPopupBlocked(true);
+      } else if (Notification.permission === 'default') {
+        this.setBrowserNotifsPopupEnabled(true);
+      }
     }
   }
 
