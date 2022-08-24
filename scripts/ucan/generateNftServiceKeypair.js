9e11663ee9a76cb712d569441ee89a5066d7951c
RustemYuzlibaev <rustem.yuzlibaev@yandex.ru>
2022-08-24 12:25:35 +0300

    add registering & change path

--- scripts/ucan/generateNftServiceKeypair.js
+++ scripts/ucan/generateNftServiceKeypair.js
@@ -1,15 +1,11 @@
 import { writeFile } from 'fs';
+import { join } from 'path';
 import fetch from 'node-fetch';
 import { KeyPair } from 'ucan-storage/keypair';
 import { build } from 'ucan-storage/ucan-storage';
 
 const SERVICE_ENDPOINT = 'https://api.nft.storage'; // default
-// const API_TOKEN = process.env.API_KEY;
-
-// console.log('process.env.API_KEY', process.env.API_KEY);
-
-const API_TOKEN =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDgwNzdkQzgxMDRCNUNCZjNhNDRlYjE2OWVFREZlREUwNmRFODM2ZTciLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2MTI2MjM4NTE4NiwibmFtZSI6InVjYW4ifQ.BGgAMQ94UcG8zLbcSmEAx3HH2r_QYhfCH4JBYonG7N8';
+const API_TOKEN = process.env.API_KEY;
 
 /**
  * Obtaining the service DID
@@ -27,9 +23,32 @@ async function getServiceDid() {
 }
 
 /**
+ * Registering marketplace DID
+ *
+ * @param {string} did
+ * @param {string} token
+ */
+async function registerMarketplaceDid(did, token) {
+  const registerRes = await fetch(new URL('/user/did', SERVICE_ENDPOINT), {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      did,
+    }),
+  });
+
+  if (!registerRes.ok) {
+    throw new Error('Failed to register marketplace DID');
+  }
+}
+
+/**
  * Obtaining a root UCAN token.
  * It will be valid for a duration of two weeks
  *
+ * @param {string} token
  */
 async function getRootToken(token) {
   const ucanReq = await fetch(new URL('/ucan/token', SERVICE_ENDPOINT), {
@@ -38,8 +57,6 @@ async function getRootToken(token) {
       Authorization: `Bearer ${token}`,
     },
   });
-
-  console.log('ucanReq', ucanReq);
 
   if (!ucanReq.ok) {
     throw new Error('Failed to get root UCAN token');
@@ -53,6 +70,9 @@ async function getRootToken(token) {
 /**
  * Obtaining UCAN token with specified expiration date (10 days)
  *
+ * @param {KeyPair} kp
+ * @param {string} serviceDid
+ * @param {string} rootUCAN
  */
 async function getUCAN(kp, serviceDid, rootUCAN) {
   // convert timestamp to seconds
@@ -86,6 +106,8 @@ async function getUCAN(kp, serviceDid, rootUCAN) {
 
     const serviceDid = await getServiceDid();
 
+    await registerMarketplaceDid(kp.did(), API_TOKEN);
+
     const rootUCAN = await getRootToken(API_TOKEN);
 
     const ucan = await getUCAN(kp, serviceDid, rootUCAN);
@@ -95,7 +117,7 @@ async function getUCAN(kp, serviceDid, rootUCAN) {
       "ucan": "${ucan}"
     }\n`;
 
-    writeFile('ucan.json', credentials, (err) => {
+    writeFile(join(process.cwd(), 'scripts/ucan', 'ucan.json'), credentials, (err) => {
       if (err) throw new Error(err);
 
       process.stdout.write('The ucan file has been saved!\n');
