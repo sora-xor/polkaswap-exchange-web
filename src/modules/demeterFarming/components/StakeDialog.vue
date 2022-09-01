b7c955472b529f98037f875c81879df3780e9217
Nikita Polyakov <53777036+Nikita-Polyakov@users.noreply.github.com>
2022-09-01 09:48:51 +0300

    update wallet to 1.10.21 & update not translated strings (#787)

--- src/modules/demeterFarming/components/StakeDialog.vue
+++ src/modules/demeterFarming/components/StakeDialog.vue
@@ -10,7 +10,7 @@
       </s-row>
 
       <div v-if="isAdding" class="stake-dialog-info">
-        <info-line label="APR" :value="aprFormatted" />
+        <info-line :label="TranslationConsts.APR" :value="aprFormatted" />
         <info-line :label="t('demeterFarming.info.totalLiquidityLocked')" :value="tvlFormatted" />
         <info-line :label="t('demeterFarming.info.rewardToken')" :value="rewardAssetSymbol" />
       </div>
