a142d98c385cd9b0d490578ae0a7f144d426f537
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2022-10-07 12:02:41 +0300

    refactoring BridgeContainer

--- src/views/BridgeContainer.vue
+++ src/views/BridgeContainer.vue
@@ -14,6 +14,7 @@ import { ethers } from 'ethers';
 import { mixins } from '@soramitsu/soraneo-wallet-web';
 
 import WalletConnectMixin from '@/components/mixins/WalletConnectMixin';
+import SubscriptionsMixin from '@/components/mixins/SubscriptionsMixin';
 import ethersUtil from '@/utils/ethers-util';
 import { action } from '@/store/decorators';
 
@@ -25,54 +26,53 @@ import { Components } from '@/consts';
     SelectNetwork: lazyComponent(Components.SelectNetwork),
   },
 })
-export default class BridgeContainer extends Mixins(mixins.LoadingMixin, WalletConnectMixin) {
+export default class BridgeContainer extends Mixins(mixins.LoadingMixin, WalletConnectMixin, SubscriptionsMixin) {
   @action.bridge.getEvmNetworkFee private getEvmNetworkFee!: AsyncVoidFn;
   @action.bridge.updateEvmBlockNumber private updateEvmBlockNumber!: (block?: number) => Promise<void>;
-  @action.bridge.subscribeOnHistory subscribeOnHistory!: VoidFunction;
-  @action.bridge.unsubscribeFromHistory unsubscribeFromHistory!: VoidFunction;
+  @action.bridge.subscribeOnHistory subscribeOnHistory!: AsyncVoidFn;
+  @action.bridge.unsubscribeFromHistory unsubscribeFromHistory!: AsyncVoidFn;
   @action.assets.updateRegisteredAssets private updateExternalBalances!: (reset?: boolean) => Promise<void>;
 
   private unwatchEthereum!: VoidFunction;
   private blockHeadersSubscriber: ethers.providers.Web3Provider | undefined;
 
   async created(): Promise<void> {
-    await this.withLoading(async () => {
-      await this.withParentLoading(async () => {
-        await this.restoreSelectedEvmNetwork();
-        await this.onConnectedEvmNetworkChange();
+    this.setStartSubscriptions([this.subscribeOnHistory, this.subscribeToEvmBlockHeaders, this.subscribeOnEvm]);
+    this.setResetSubscriptions([this.unsubscribeFromHistory, this.unsubscribeEvmBlockHeaders, this.unsubscribeFromEvm]);
 
-        this.unwatchEthereum = await ethersUtil.watchEthereum({
-          onAccountChange: (addressList: string[]) => {
-            if (addressList.length) {
-              this.switchExternalAccount(addressList[0]);
-              this.updateExternalBalances();
-            } else {
-              this.disconnectExternalAccount();
-            }
-          },
-          onNetworkChange: (networkHex: string) => {
-            this.onConnectedEvmNetworkChange(networkHex);
-          },
-          onDisconnect: () => {
-            this.disconnectExternalAccount();
-          },
-        });
-        this.subscribeToEvmBlockHeaders();
-        this.subscribeOnHistory();
-      });
+    await this.withParentLoading(async () => {
+      await this.restoreSelectedEvmNetwork();
+      await this.onConnectedEvmNetworkChange();
     });
-  }
-
-  beforeDestroy(): void {
-    if (typeof this.unwatchEthereum === 'function') {
-      this.unwatchEthereum();
-    }
-    this.unsubscribeEvmBlockHeaders();
-    this.unsubscribeFromHistory();
   }
 
   private async onConnectedEvmNetworkChange(networkHex?: string) {
     await Promise.all([this.connectEvmNetwork(networkHex), this.updateExternalBalances(true), this.getEvmNetworkFee()]);
+  }
+
+  private async subscribeOnEvm(): Promise<void> {
+    this.unwatchEthereum = await ethersUtil.watchEthereum({
+      onAccountChange: (addressList: string[]) => {
+        if (addressList.length) {
+          this.switchExternalAccount(addressList[0]);
+          this.updateExternalBalances();
+        } else {
+          this.disconnectExternalAccount();
+        }
+      },
+      onNetworkChange: (networkHex: string) => {
+        this.onConnectedEvmNetworkChange(networkHex);
+      },
+      onDisconnect: () => {
+        this.disconnectExternalAccount();
+      },
+    });
+  }
+
+  private async unsubscribeFromEvm(): Promise<void> {
+    if (typeof this.unwatchEthereum === 'function') {
+      this.unwatchEthereum();
+    }
   }
 
   private async subscribeToEvmBlockHeaders(): Promise<void> {
