1fa6737e8aadfd793c760bbfa62cdc456d2fd1cf
RustemYuzlibaev <rustem.yuzlibaev@yandex.ru>
2023-01-24 10:09:11 +0300

    roll out prod widget X1

--- src/components/X1/X1Dialog.vue
+++ src/components/X1/X1Dialog.vue
@@ -70,7 +70,8 @@ export default class X1Dialog extends Mixins(mixins.DialogMixin, mixins.LoadingM
   }
 
   mounted(): void {
-    this.X1Widget = X1Api.getWidget(this.soraNetwork);
+    this.X1Widget = X1Api.getWidget('Prod'); // TODO: change to this.soraNetwork later
+    console.log('this.here', this.X1Widget);
   }
 }
 </script>
