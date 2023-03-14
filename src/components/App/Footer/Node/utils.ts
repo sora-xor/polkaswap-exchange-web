a98fccc2b1a8eb91d115e155448b3679471d5a8b
Stefan Popov <17688105+stefashkaa@users.noreply.github.com>
2023-03-14 22:26:56 +0100

    Fix displayRegions

--- src/components/App/Footer/Node/utils.ts
+++ src/components/App/Footer/Node/utils.ts
@@ -6,7 +6,11 @@ export function formatLocation(code: string): string {
   try {
     const isoCode = code.toUpperCase();
     const flag = countryCodeEmoji(isoCode);
-    const name = store.state.settings.displayRegions?.of(isoCode);
+    const displayRegions = store.state.settings.displayRegions;
+    if (!displayRegions) {
+      return flag;
+    }
+    const name = displayRegions.of(isoCode);
     return name ? `${name} ${flag}` : flag;
   } catch (error) {
     console.warn('Unsupported format of location in env.json', error);
