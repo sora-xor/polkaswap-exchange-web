a3135c96186763d9ab5710bae3570c1bad1d2b51
RustemYuzlibaev <rustem.yuzlibaev@yandex.ru>
2023-04-26 10:48:19 +0300

    up wallet

--- tests/mocks/account.ts
+++ tests/mocks/account.ts
@@ -1,8 +1,8 @@
 import { PolkadotJsAccount } from '@soramitsu/soraneo-wallet-web/lib/types/common';
-import { Extensions } from '@soramitsu/soraneo-wallet-web/lib/consts';
+import { AppWallet } from '@soramitsu/soraneo-wallet-web/lib/consts';
 
 export const MOCK_ACCOUNT: PolkadotJsAccount = {
   address: 'cnRXua6zs8TaE87BQFL6uWVbT2g6GXsUjwk6PTvL6UHcHDCvo',
   name: 'Sora Name',
-  source: Extensions.PolkadotJS,
+  source: AppWallet.PolkadotJS,
 };
