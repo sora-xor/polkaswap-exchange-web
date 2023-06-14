ebd67c94387f574d2b252ee336ae1dcdcb607485
RustemYuzlibaev <rustem.yuzlibaev@yandex.ru>
2023-06-14 08:58:12 +0300

    adjust icons for ads

--- src/components/App/Header/AppAd.vue
+++ src/components/App/Header/AppAd.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="ad">
-    <div v-if="showSoraCardAd" class="ad-sora-card" @click="goTo(PageNames.SoraCard)">
+    <div v-if="showSoraCardAd" @click="goTo(PageNames.SoraCard)" class="ad-card-banner banner">
       <span class="ad-call-to-action">
         {{ 'Get SORA Card' }}
-        <s-icon name="arrows-arrow-top-right-24" />
+        <s-icon name="arrows-arrow-top-right-24" size="20" />
       </span>
-      <sora-card-banner />
+      <card />
     </div>
-    <div v-if="showXstArticle" class="ad-xst-article" />
+    <div v-if="showXstArticle" @click="openArticle" class="ad-xst-banner banner">
+      <span class="ad-call-to-action">
+        {{ 'Learn of XST' }}
+        <s-icon name="arrows-arrow-top-right-24" size="20" />
+      </span>
+      <xst />
+    </div>
   </div>
 </template>
 
@@ -15,14 +21,16 @@
 import { mixins } from '@soramitsu/soraneo-wallet-web';
 import { Component, Mixins, Watch } from 'vue-property-decorator';
 
-import SoraCardBanner from '@/assets/img/ads/soracard.svg?inline';
+import Card from '@/assets/img/ads/card.svg?inline';
+import Xst from '@/assets/img/ads/xst.svg?inline';
 import { PageNames } from '@/consts';
 import { goTo } from '@/router';
 import { getter } from '@/store/decorators';
 
 @Component({
   components: {
-    SoraCardBanner,
+    Xst,
+    Card,
   },
 })
 export default class AppAd extends Mixins(mixins.TranslationMixin) {
@@ -42,36 +50,54 @@ export default class AppAd extends Mixins(mixins.TranslationMixin) {
   probability = () => Math.random();
 
   chooseAdToShow(): void {
+    this.showSoraCardAd = false;
+    this.showXstArticle = false;
+
     // TODO: add check if already applied for SORA Card
     if (this.soraCardEnabled) {
-      // 50% chance to show SORA Card ad
-      if (this.probability() < 0.5) {
+      // 20% chance to show SORA Card ad
+      if (this.probability() > 0.5) {
         this.showSoraCardAd = true;
+        return;
       }
-      // 80% chance to show XST ad
-    } else if (this.probability() > 0.2) {
+    }
+
+    // 10% chance to show XST ad
+    if (this.probability() > 0.1) {
       this.showXstArticle = true;
     }
+  }
+
+  openArticle(): void {
+    //
   }
 }
 </script>
 
 <style lang="scss" scoped>
 .ad {
+  .banner {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 43px;
+    border-radius: 32px;
+  }
+  &-card-banner {
+    background: linear-gradient(89.07deg, #f8087b -45.39%, #ee2233 138.94%);
+    box-shadow: -5px -5px 10px var(--s-color-base-on-accent), 1px 1px 10px rgba(0, 0, 0, 0.1),
+      inset 1px 1px 2px rgba(255, 255, 255, 0.8);
+  }
+
+  &-xst-banner {
+    background: linear-gradient(89.07deg, #f8087b -45.39%, #ff408c 138.94%);
+  }
+
   &-call-to-action {
-    position: absolute;
     color: #fff;
-    font-weight: 700;
-    position: absolute;
-    width: 151px;
-    height: 27px;
-    left: 4%;
-    top: calc(50% - 27px / 2);
-    font-style: normal;
     font-weight: 600;
     font-size: 18px;
-    line-height: 150%;
-    text-align: center;
+    margin-left: 16px;
     letter-spacing: -0.02em;
     text-transform: uppercase;
   }
@@ -81,8 +107,10 @@ export default class AppAd extends Mixins(mixins.TranslationMixin) {
   }
 
   .s-icon-arrows-arrow-top-right-24 {
-    position: absolute;
-    margin-left: 3px;
+    position: relative;
+    bottom: -2px;
+    margin-left: 2px;
+    margin-right: 14px;
     color: #fff;
   }
 }
