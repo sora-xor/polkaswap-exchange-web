392923f3e6ffca206ee8d73bc6948321a436a178
RustemYuzlibaev <rustem.yuzlibaev@yandex.ru>
2023-08-29 10:47:41 +0300

    Update X1Dialog.vue

--- src/components/pages/X1/X1Dialog.vue
+++ src/components/pages/X1/X1Dialog.vue
@@ -1,10 +1,8 @@
 <template>
   <dialog-base :visible.sync="isVisible" class="x1-dialog">
-    <template #title>
-      <x1ex-logo :theme="libraryTheme" />
-    </template>
     <div class="wrapper" v-loading="loadingX1">
       <div
+        v-if="!showErrorInfoBanner"
         :id="widgetId"
         data-from-currency="EUR"
         :data-address="accountAddress"
@@ -13,6 +11,21 @@
         :data-hide-try-again-button="false"
         data-locale="en"
       />
+      <div v-else class="x1-error-info-banner">
+        <s-icon class="x1-error-info-banner__icon" name="basic-clear-X-24" size="64px" />
+        <h4 class="x1-error-info-banner__header">The payment widget is currently unavailable</h4>
+        <p class="x1-error-info-banner__text">
+          Apologies for the inconvenience. We're working diligently to resolve this. Please, try again later.
+        </p>
+        <s-button
+          class="x1-error-info-banner__btn s-typography-button--large"
+          type="primary"
+          :disabled="loading"
+          @click="closeDialog"
+        >
+          {{ t('browserNotificationDialog.agree') }}
+        </s-button>
+      </div>
     </div>
   </dialog-base>
 </template>
@@ -21,28 +34,21 @@
 import { components, mixins, WALLET_CONSTS, ScriptLoader } from '@soramitsu/soraneo-wallet-web';
 import { Component, Mixins, Watch } from 'vue-property-decorator';
 
-import X1exLogo from '../../../components/shared/Logo/X1ex.vue';
-import { getter, state } from '../../../store/decorators';
-import { delay } from '../../../utils';
-import { X1Api, X1Widget } from '../../../utils/x1';
-
-import type Theme from '@soramitsu/soramitsu-js-ui/lib/types/Theme';
+import { getter, state } from '@/store/decorators';
+import { X1Api, X1Widget } from '@/utils/x1';
 
 @Component({
   components: {
     DialogBase: components.DialogBase,
-    X1exLogo,
   },
 })
 export default class X1Dialog extends Mixins(mixins.DialogMixin, mixins.LoadingMixin, mixins.TranslationMixin) {
   @state.soraCard.euroBalance private euroBalance!: string;
-  @state.wallet.settings.soraNetwork soraNetwork!: WALLET_CONSTS.SoraNetwork;
-
+  @state.wallet.settings.soraNetwork soraNetwork!: Nullable<WALLET_CONSTS.SoraNetwork>;
   @getter.soraCard.accountAddress accountAddress!: string;
-  @getter.settings.x1Enabled x1Enabled!: boolean;
-  @getter.libraryTheme libraryTheme!: Theme;
 
   X1Widget: X1Widget = { sdkUrl: '', widgetId: '' };
+  showErrorInfoBanner = false;
   loadingX1 = false;
 
   @Watch('isVisible', { immediate: true })
@@ -55,6 +61,14 @@ export default class X1Dialog extends Mixins(mixins.DialogMixin, mixins.LoadingM
     }
   }
 
+  @Watch('soraNetwork', { immediate: true })
+  private checkEnvConfig(value: Nullable<WALLET_CONSTS.SoraNetwork>): void {
+    if (!value) {
+      return; // if env config is not loaded
+    }
+    this.X1Widget = X1Api.getWidget(value);
+  }
+
   get restEuroToDeposit(): number {
     return 100 - parseInt(this.euroBalance, 10);
   }
@@ -65,31 +79,17 @@ export default class X1Dialog extends Mixins(mixins.DialogMixin, mixins.LoadingM
 
   async loadX1(): Promise<void> {
     try {
-      await ScriptLoader.load(this.X1Widget.sdkUrl, true);
+      await ScriptLoader.load(this.X1Widget.sdkUrl, false);
     } catch {
-      this.closeWidget(true);
+      this.showErrorInfoBanner = true;
+      this.loadingX1 = false;
     } finally {
-      await delay(1500);
       this.loadingX1 = false;
     }
   }
 
   unloadX1(): void {
-    ScriptLoader.unload(this.X1Widget.sdkUrl, false);
-  }
-
-  closeWidget(error = false): void {
-    this.loadingX1 = false;
-    this.unloadX1();
-    this.closeDialog();
-    if (error) {
-      this.$emit('error');
-    }
-  }
-
-  mounted(): void {
-    if (!this.x1Enabled) this.closeWidget();
-    this.X1Widget = X1Api.getWidget(this.soraNetwork);
+    ScriptLoader.unload(this.X1Widget.sdkUrl, false).catch(() => {});
   }
 }
 </script>
@@ -98,12 +98,7 @@ export default class X1Dialog extends Mixins(mixins.DialogMixin, mixins.LoadingM
 .x1-dialog .el-dialog .wrapper {
   min-height: 320px;
   padding: $basic-spacing-medium;
-  margin: -10px -20px;
-  border-radius: var(--s-border-radius-base);
-
-  .el-loading-mask {
-    border-radius: 20px;
-  }
+  margin: -10px -20px -20px;
 }
 </style>
 
@@ -127,10 +122,43 @@ export default class X1Dialog extends Mixins(mixins.DialogMixin, mixins.LoadingM
   }
 
   ul {
-    margin-top: $basic-spacing / 2;
+    margin-top: $inner-spacing-mini;
   }
 }
 [design-system-theme='dark'] .disclaimer-warning-icon .s-icon-notifications-alert-triangle-24 {
   color: var(--s-color-base-content-primary);
+}
+
+.x1-error-info-banner {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  &__header {
+    font-size: var(--s-heading3-font-size);
+    font-weight: 500;
+  }
+
+  &__text {
+    margin-top: var(--s-size-mini);
+    line-height: var(--s-font-size-large);
+    font-size: var(--s-font-size-medium);
+    font-weight: 300;
+    width: 67%;
+  }
+
+  &__icon {
+    display: block;
+    color: var(--s-color-status-error);
+    width: var(--s-size-mini);
+    margin: -20px 20px $basic-spacing 0;
+  }
+
+  &__btn {
+    margin-top: var(--s-size-mini);
+    width: 100%;
+  }
 }
 </style>
