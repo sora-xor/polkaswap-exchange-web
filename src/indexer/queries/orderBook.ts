f9fd679be06f18576a20de7e26e84d830239eb7c
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2023-11-15 09:04:21 +0300

    account orders request

--- src/indexer/queries/orderBook.ts
+++ src/indexer/queries/orderBook.ts
@@ -3,7 +3,10 @@ import { getCurrentIndexer, WALLET_CONSTS } from '@soramitsu/soraneo-wallet-web'
 import { SubqueryIndexer, SubsquidIndexer } from '@soramitsu/soraneo-wallet-web/lib/services/indexer';
 import { gql } from '@urql/core';
 
-import type { SubqueryConnectionQueryResponse } from '@soramitsu/soraneo-wallet-web/lib/services/indexer/subquery/types';
+import type {
+  SubqueryConnectionQueryResponse,
+  SubquerySubscriptionPayload,
+} from '@soramitsu/soraneo-wallet-web/lib/services/indexer/subquery/types';
 import type {
   OrderBookEntity,
   OrderBookLimitOrderEntity,
@@ -78,6 +81,18 @@ export async function fetchOrderBooks(): Promise<Nullable<OrderBookData[]>> {
   return null;
 }
 
+type OrderData = {
+  timestamp: number;
+  isBuy: boolean;
+  price: FPNumber;
+  amount: FPNumber;
+  amountFilled: FPNumber;
+  orderId: number | undefined;
+  lifetime: number;
+  expiresAt: number;
+  status: string;
+};
+
 const SubqueryAccountMarketOrdersQuery = gql<SubqueryConnectionQueryResponse<OrderBookMarketOrderEntity>>`
   query AccountMarketOrdersQuery($after: Cursor, $orderBookId: String, $accountId: String) {
     data: orderBookMarketOrders(
@@ -137,9 +152,68 @@ const SubqueryAccountLimitOrdersQuery = gql<SubqueryConnectionQueryResponse<Orde
   }
 `;
 
+const parseOrderEntity = (item: OrderBookMarketOrderEntity | OrderBookLimitOrderEntity): OrderData => {
+  return {
+    timestamp: item.timestamp * 1000,
+    isBuy: item.isBuy,
+    price: new FPNumber(item.price),
+    amount: new FPNumber(item.amount),
+    amountFilled: new FPNumber('amountFilled' in item ? item.amountFilled : item.amount),
+    orderId: 'orderId' in item ? item.orderId : undefined,
+    lifetime: ('lifetime' in item ? item.lifetime : 0) * 1000,
+    expiresAt: ('expiresAt' in item ? item.expiresAt : item.timestamp) * 1000,
+    status: 'status' in item ? item.status : 'Filled',
+  };
+};
+
 export async function fetchOrderBookAccountOrders(
   dexId: number,
   baseAssetId: string,
   quoteAssetId: string,
   accountAddress: string
-) {}
+): Promise<Nullable<OrderData[]>> {
+  const orderBookId = [dexId, baseAssetId, quoteAssetId].join('-');
+  const variables = { orderBookId, accountId: accountAddress };
+  const indexer = getCurrentIndexer();
+
+  switch (indexer.type) {
+    case IndexerType.SUBQUERY: {
+      const subqueryIndexer = indexer as SubqueryIndexer;
+      const [limitOrders, marketOrders] = await Promise.all([
+        subqueryIndexer.services.explorer.fetchAllEntities(
+          SubqueryAccountLimitOrdersQuery,
+          variables,
+          parseOrderEntity
+        ),
+        subqueryIndexer.services.explorer.fetchAllEntities(
+          SubqueryAccountMarketOrdersQuery,
+          variables,
+          parseOrderEntity
+        ),
+      ]);
+
+      return [...(limitOrders || []), ...(marketOrders || [])].sort((a, b) => +b.timestamp - +a.timestamp);
+    }
+  }
+
+  return null;
+}
+
+/* eslint-disable camelcase */
+type OrderBookEntityMutation = {
+  price: string;
+  price_change_day: number;
+  volume_day_usd: string;
+  last_deals: string;
+};
+/* eslint-enable camelcase */
+
+const OrderBookSubscription = gql<SubquerySubscriptionPayload<OrderBookEntityMutation>>`
+  subscription SubqueryOrderBookSubscription($id: ID!) {
+    payload: orderBook(id: $id, mutation: [UPDATE]) {
+      id
+      mutation_type
+      _entity
+    }
+  }
+`;
