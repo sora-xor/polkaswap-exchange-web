481d6b430b2cb3950d40445aa3fc2a6ae08e0868
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2024-02-13 13:56:07 +0300

    move action to class

--- src/utils/connection/index.ts
+++ src/utils/connection/index.ts
@@ -1,4 +1,5 @@
-import type { Node } from '@/types/nodes';
+import type { Node, ConnectToNodeOptions } from '@/types/nodes';
+import { AppHandledError } from '@/utils/error';
 import { fetchRpc, getRpcEndpoint } from '@/utils/rpc';
 
 import type { Connection } from '@sora-substrate/connection';
@@ -100,7 +101,7 @@ class NodesConnection {
     this.chainGenesisHash = hash || '';
   }
 
-  async updateNetworkChainGenesisHash(): Promise<void> {
+  protected async updateNetworkChainGenesisHash(): Promise<void> {
     try {
       const genesisHash = await Promise.any(this.defaultNodes.map((node) => this.getChainId(node.address)));
       this.setNetworkChainGenesisHash(genesisHash);
@@ -115,12 +116,137 @@ class NodesConnection {
     return id;
   }
 
-  async closeConnectionWithInfo() {
+  protected async closeConnectionWithInfo() {
     const { endpoint, opened } = this.connection;
 
     if (endpoint && opened) {
       await this.connection.close();
       console.info('[SORA] Disconnected from node', endpoint);
+    }
+  }
+
+  async connectToNode(options: ConnectToNodeOptions = {}): Promise<void> {
+    const { node, onError, currentNodeIndex = 0, ...restOptions } = options;
+
+    const defaultNode = this.nodeList[currentNodeIndex];
+    const requestedNode = (node || (this.node?.address ? this.node : defaultNode)) as Node;
+
+    try {
+      await this.setNode({ node: requestedNode, onError, ...restOptions });
+    } catch (error) {
+      onError?.(error, requestedNode);
+
+      // if connection failed to node in state, reset node in state
+      if (requestedNode && requestedNode.address === this.node?.address) {
+        this.resetNode();
+      }
+
+      // loop through the node list
+      if (this.node?.address || currentNodeIndex !== this.defaultNodes.length - 1) {
+        const nextIndex = requestedNode?.address === defaultNode.address ? currentNodeIndex + 1 : 0;
+        await this.connectToNode({ onError, currentNodeIndex: nextIndex, ...restOptions });
+      }
+
+      throw error;
+    }
+  }
+
+  async setNode(options: ConnectToNodeOptions = {}): Promise<void> {
+    const { node, connectionOptions = {}, onError, onDisconnect, onReconnect } = options;
+
+    const endpoint = node?.address ?? '';
+    const isTrustedEndpoint = !!this.defaultNodes.find((node) => node.address === endpoint);
+    const connectionOpenOptions = {
+      once: true, // by default we are trying to connect once, but keep trying after disconnect from connected node
+      timeout: NODE_CONNECTION_TIMEOUT,
+      ...connectionOptions,
+    };
+    const isReconnection = !connectionOpenOptions.once;
+    const connectingNodeChanged = () => endpoint !== this.nodeAddressConnecting;
+
+    const connectionOnDisconnected = async () => {
+      await this.closeConnectionWithInfo();
+
+      if (typeof onDisconnect === 'function') {
+        onDisconnect(node as Node);
+      }
+
+      this.connectToNode({
+        node,
+        onError,
+        onDisconnect,
+        onReconnect,
+        connectionOptions: { ...connectionOpenOptions, once: false },
+      });
+    };
+
+    const connectionOnReady = () => {
+      this.connection.addEventListener('disconnected', connectionOnDisconnected);
+    };
+
+    try {
+      if (!endpoint) {
+        throw new Error('Node address is not set');
+      }
+
+      this.setNodeRequest(node!);
+
+      console.info('[SORA] Connection request to node', endpoint);
+
+      await this.closeConnectionWithInfo();
+
+      await this.connection.open(endpoint, {
+        ...connectionOpenOptions,
+        eventListeners: [['ready', connectionOnReady]],
+      });
+
+      if (connectingNodeChanged()) return;
+
+      console.info('[SORA] Connected to node', this.connection.endpoint);
+
+      const nodeChainGenesisHash = this.connection.api?.genesisHash.toHex();
+
+      if (!isTrustedEndpoint) {
+        // if genesis hash is not set in state, fetch it
+        if (!this.chainGenesisHash) {
+          await this.updateNetworkChainGenesisHash();
+        }
+
+        if (this.chainGenesisHash && nodeChainGenesisHash !== this.chainGenesisHash) {
+          // disconnect from node to prevent network subscriptions activation
+          await this.closeConnectionWithInfo();
+
+          throw new AppHandledError(
+            {
+              key: 'node.errors.network',
+              payload: { address: endpoint },
+            },
+            `Chain genesis hash doesn't match: "${nodeChainGenesisHash}" received, should be "${this.chainGenesisHash}"`
+          );
+        }
+      } else {
+        this.setNetworkChainGenesisHash(nodeChainGenesisHash);
+      }
+
+      if (isReconnection) {
+        onReconnect?.(node as Node);
+      }
+
+      this.setNodeSuccess(node!);
+    } catch (error) {
+      console.error(error);
+      const err =
+        error instanceof AppHandledError
+          ? error
+          : new AppHandledError({
+              key: 'node.errors.connection',
+              payload: { address: endpoint },
+            });
+
+      if (!connectingNodeChanged()) {
+        this.setNodeFailure();
+      }
+      throw err;
     }
   }
 }
