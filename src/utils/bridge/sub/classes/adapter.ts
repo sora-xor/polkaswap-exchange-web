4fd633f26ca188eb24f5d2d4c93fd5834e6439db
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2024-02-23 08:21:42 +0300

    update polkadot treasury fee

--- src/utils/bridge/sub/classes/adapter.ts
+++ src/utils/bridge/sub/classes/adapter.ts
@@ -210,8 +210,8 @@ class RelaychainAdapter extends SubAdapter {
           // Hardcoded value for Kusama - 0.002 KSM (0.0007 KSM transaction fee + 0.0013 KSM treasury)
           return '2000000000';
         case SubNetworkId.Polkadot:
-          // Hardcoded value for Polkadot - 0.01836 DOT
-          return '183600000';
+          // Hardcoded value for Polkadot - 0.059 DOT (0.019 DOT transaction fee + 0.04 DOT treasury)
+          return '590000000';
         default:
           return '0';
       }
