bfbcb88828b17926a24e44700e016963a68de610
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2024-05-08 15:27:51 +0300

    update packages

--- src/components/shared/Widget/PriceChart.vue
+++ src/components/shared/Widget/PriceChart.vue
@@ -835,8 +835,8 @@ export default class PriceChartWidget extends Mixins(
     if (this.isOrderBook) {
       return await subscribeOnOrderBookUpdates(
         this.dexId,
-        this.baseAsset!.address,
-        this.quoteAsset!.address,
+        (this.baseAsset as AccountAsset).address,
+        (this.quoteAsset as AccountAsset).address,
         () => this.fetchAndHandleUpdate(entities),
         console.error
       );
