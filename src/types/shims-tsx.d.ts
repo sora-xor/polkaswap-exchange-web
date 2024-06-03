ac706d27f211b1d9b2de56dd9037c0f6ae511a04
RustemYuzlibaev <rustem.yuzlibaev@yandex.ru>
2024-06-03 10:27:22 +0300

    Update shims-tsx.d.ts

--- src/types/shims-tsx.d.ts
+++ src/types/shims-tsx.d.ts
@@ -1,6 +1,10 @@
+import { Telegram } from '@twa-dev/types';
 import Vue, { VNode } from 'vue';
 
 declare global {
+  interface Window {
+    Telegram: Telegram;
+  }
   namespace JSX {
     // tslint:disable no-empty-interface
     interface Element extends VNode {}
