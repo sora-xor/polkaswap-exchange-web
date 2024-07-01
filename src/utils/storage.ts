ace297ee3b92435b30e16fff85648d07232fe4a0
sorabot <admin@soramitsu.co.jp>
2024-07-01 06:32:09 +0000

    copy

--- src/utils/storage.ts
+++
@@ -1,8 +0,0 @@
-import { Storage } from '@sora-substrate/util';
-import { storage as soraStorage } from '@soramitsu/soraneo-wallet-web';
-
-export { settingsStorage } from '@soramitsu/soraneo-wallet-web';
-
-export default soraStorage;
-
-export const layoutsStorage = new Storage('layouts');
--- tests/mocks/account.ts
+++
@@ -1,8 +0,0 @@
-import { AppWallet } from '@soramitsu/soraneo-wallet-web/lib/consts';
-import { PolkadotJsAccount } from '@soramitsu/soraneo-wallet-web/lib/types/common';
-
-export const MOCK_ACCOUNT: PolkadotJsAccount = {
-  address: 'cnRXua6zs8TaE87BQFL6uWVbT2g6GXsUjwk6PTvL6UHcHDCvo',
-  name: 'Sora Name',
-  source: AppWallet.PolkadotJS,
-};
