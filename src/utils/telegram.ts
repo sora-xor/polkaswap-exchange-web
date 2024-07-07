f1c88e0017e1f4efd6663334fa7cdb4c7695bf3b
Stefan Popov <17688105+stefashkaa@users.noreply.github.com>
2024-07-07 12:41:46 +0200

    Update telegram.ts

--- src/utils/telegram.ts
+++ src/utils/telegram.ts
@@ -2,70 +2,18 @@ import { api } from '@soramitsu/soraneo-wallet-web';
 import {
   setDebug,
   initViewport,
-  initInitData,
   initMiniApp,
   initWeb,
   isIframe,
+  retrieveLaunchParams,
+  initUtils,
   type MiniApp,
   type LaunchParams,
   type Utils,
   type Viewport,
-  retrieveLaunchParams,
-  initUtils,
 } from '@tma.js/sdk';
 
 import store from '@/store';
-
-/**
- * Update the theme of the Telegram Mini App using `var(--s-color-utility-body)`
- */
-function updateTgTheme(): void {
-  try {
-    const [miniApp] = initMiniApp();
-    const colorUtilityBody =
-      (getComputedStyle(document.documentElement).getPropertyValue('--s-color-utility-body') as `#${string}`) ||
-      '#f7f3f4'; // Default color
-
-    miniApp.setHeaderColor(colorUtilityBody);
-    miniApp.setBgColor(colorUtilityBody);
-  } catch (error) {
-    console.warn('[TMA]: updateTgTheme', error);
-  }
-}
-
-async function initTMA(botUrl?: string, isDebug = false): Promise<void> {
-  try {
-    // Initialize the app in the web version of Telegram
-    if (isIframe()) {
-      initWeb();
-      console.info('[TMA]: initTMA: Web version of Telegram');
-    }
-    // Expand the Mini App to the maximum available height
-    const [viewport] = initViewport();
-    const viewportInstance = await viewport;
-    viewportInstance.expand();
-    console.info('[TMA]: initTMA: Expand the Mini App to the maximum available height');
-    // Set theme
-    updateTgTheme();
-    console.info('[TMA]: initTMA: Set theme');
-    // Enable debugging
-    setDebug(isDebug);
-    // Check the referrer
-    const initData = initInitData();
-    console.info('[TMA]: initTMA: Data initialization', initData);
-    const referrerAddress = initData?.startParam;
-
-    if (referrerAddress && api.validateAddress(referrerAddress)) {
-      store.commit.referrals.setStorageReferrer(referrerAddress);
-    }
-    // Set the Telegram bot URL
-    if (botUrl) {
-      store.commit.settings.setTelegramBotUrl(botUrl);
-    }
-  } catch (error) {
-    console.warn('[TMA]: initTMA', error);
-  }
-}
 
 export class TmaSdk {
   public static miniApp: MiniApp;
@@ -80,33 +28,38 @@ export class TmaSdk {
         initWeb();
         console.info('[TMA]: initTMA: Web version of Telegram');
       }
+      // Init viewport
+      await this.initViewport();
       // Init mini app
       const [miniApp] = initMiniApp();
       this.miniApp = miniApp;
       console.info('[TMA]: Mini app was initialized');
+      // Set theme
+      this.updateTheme();
+      // Enable debugging
+      setDebug(isDebug);
       // Retrieve launch params
       this.launchParams = retrieveLaunchParams();
       console.info('[TMA]: Launch params were retrieved');
-      // Init utils
-      this.utils = initUtils();
-      console.info('[TMA]: Utils were initialized');
-      // Init viewport
-      await this.initViewport();
-      // Enable debugging
-      setDebug(isDebug);
-      // Set theme
-      this.updateTheme();
       // Check the referrer
       this.setReferrer(this.launchParams.startParam);
       // Set the Telegram bot URL
       if (botUrl) {
         store.commit.settings.setTelegramBotUrl(botUrl);
       }
+      // Init utils
+      this.utils = initUtils();
+      console.info('[TMA]: Utils were initialized');
     } catch (error) {
       console.warn('[TMA]: init', error);
     }
   }
 
+  /**
+   * **Should be used after `miniApp` init!**
+   *
+   * Update the theme of the Telegram Mini App using `var(--s-color-utility-body)`
+   */
   public static updateTheme(): void {
     try {
       const colorUtilityBody =
