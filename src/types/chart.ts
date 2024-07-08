81d945061e0c8593eb378f424162b11a19c0654a
sorabot <admin@soramitsu.co.jp>
2024-07-08 09:23:07 +0000

    copy

--- src/types/chart.ts
+++
@@ -1,8 +0,0 @@
-/** "open", "close", "low", "high" data */
-export type OCLH = [number, number, number, number];
-
-export type SnapshotItem = {
-  timestamp: number;
-  price: OCLH;
-  volume: number;
-};
