a21d6a821d631750c41ba3346c5fb68fdadcd7c6
Kron1749 <70746258+Kron1749@users.noreply.github.com>
2024-07-16 12:22:37 +0300

    change_width_for_token_column (#1466)

--- src/views/Explore/Tokens.vue
+++ src/views/Explore/Tokens.vue
@@ -10,7 +10,7 @@
       class="explore-table"
     >
       <!-- Index -->
-      <s-table-column min-width="280" label="#">
+      <s-table-column min-width="290" label="#">
         <template #header>
           <div class="explore-table-item-index">
             <span @click="handleResetSort" :class="['explore-table-item-index--head', { active: isDefaultSort }]">
