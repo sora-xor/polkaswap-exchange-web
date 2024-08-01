05a95d1c83befb40307fc8e7b33cb70ebef83d17
sorabot <admin@soramitsu.co.jp>
2024-08-01 15:56:04 +0000

    copy

--- src/modules/dashboard/types/index.ts
+++
@@ -1,4 +0,0 @@
-import type { CodecString } from '@sora-substrate/util';
-import type { Asset } from '@sora-substrate/util/build/assets/types';
-
-export type OwnedAsset = Asset & { fiat: Nullable<CodecString> };
