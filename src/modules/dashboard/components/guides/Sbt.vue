3976859cf0d7aeba0e2f3ab1e7c84b082b9557c9
RustemYuzlibaev <rustem.yuzlibaev@yandex.ru>
2024-09-15 10:42:01 +0300

    remove unused

--- src/modules/dashboard/components/guides/Sbt.vue
+++ src/modules/dashboard/components/guides/Sbt.vue
@@ -76,7 +76,7 @@ export default class SbtGuide extends Mixins(mixins.TranslationMixin) {}
   &-warning.icon {
     position: absolute;
     background-color: #479aef;
-    border: 2.25257px solid #f7f3f4;
+    border: 2px solid #f7f3f4;
     box-shadow: var(--s-shadow-element-pressed);
     top: 20px;
     right: 20px;
