31bf04dc9c5d68a78db1b51924c8f04f97c1eb1e
Kron1749 <70746258+Kron1749@users.noreply.github.com>
2024-10-25 09:46:39 +0300

    fixed_route_with_wrapper

---
+++ src/views/PointSystemWrapper.vue
@@ -0,0 +1,22 @@
+<template>
+  <component :is="componentToRender" />
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-property-decorator';
+
+import store from '@/store';
+
+const PointSystemComponent = () => import('@/views/PointSystem.vue');
+const PointSystemV2Component = () => import('@/views/PointSystemV2.vue');
+
+export default class PointSystemWrapper extends Vue {
+  get componentToRender() {
+    if (store.state.settings.featureFlags.pointSystemV2) {
+      return PointSystemV2Component;
+    } else {
+      return PointSystemComponent;
+    }
+  }
+}
+</script>
